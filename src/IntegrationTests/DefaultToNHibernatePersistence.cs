bd7b98f14e44be91f1e6fe7d1401c75f72b116d8
Simon Cropp <simon.cropp@gmail.com>
2013-09-12 10:27:16 +1000

    copy source from NSB main repo
    as at
    https://github.com/Particular/NServiceBus/commit/b5d1f5271ae87ad24d3aa48f12291a77e7d829c2

---
+++ src/IntegrationTests/DefaultToNHibernatePersistence.cs
@@ -0,0 +1,13 @@
+﻿namespace SqlServer.IntegrationTests
+{
+    using NServiceBus;
+    using NServiceBus.Persistence.NHibernate;
+
+    public class DefaultToNHibernatePersistence : INeedInitialization
+    {
+        public void Init()
+        {
+            NHibernatePersistence.UseAsDefault();
+        }
+    }
+}
