a1b15d02366e4302e67ef73b77e64e03e9592643
John Simons <john@thesimonshouse.com>
2013-11-14 14:22:09 +1100

    Added integration tests

--- src/NServiceBus.SqlServer.AcceptanceTests/Properties/AssemblyInfo.cs
+++ src/NServiceBus.SqlServer.AcceptanceTests/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlServer.MyEndpoint")]
+[assembly: AssemblyTitle("NServiceBus.IntegrationTests.Automated")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlServer.MyEndpoint")]
+[assembly: AssemblyProduct("NServiceBus.IntegrationTests.Automated")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1bb528a6-cb57-47dc-ba8d-7ab5c0de9623")]
+[assembly: Guid("eebba921-499c-46ab-ac43-56aaf1af7947")]
 
 // Version information for an assembly consists of the following four values:
 //
