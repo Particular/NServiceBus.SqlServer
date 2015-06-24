5dfa2327e640b2d336a10d748e0f1b38687e969c
Mauro Servienti <mauro@topics.it>
2015-06-24 09:00:03 +0200

    adding acceptance tests

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/EndpointTemplates/ContextAppender.cs
@@ -0,0 +1,35 @@
+﻿namespace NServiceBus.AcceptanceTests.EndpointTemplates
+{
+    using AcceptanceTesting;
+    using AcceptanceTesting.Support;
+    using log4net.Appender;
+    using log4net.Core;
+
+    public class ContextAppender : AppenderSkeleton
+    {
+        public ContextAppender(ScenarioContext context, EndpointConfiguration endpointConfiguration)
+        {
+            this.context = context;
+            this.endpointConfiguration = endpointConfiguration;
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (!endpointConfiguration.AllowExceptions && loggingEvent.ExceptionObject != null)
+            {
+                lock (context)
+                {
+                    context.Exceptions += loggingEvent.ExceptionObject + "/n/r";
+                }
+            }
+            if (loggingEvent.Level >= Level.Warn)
+            {
+                context.RecordLog(endpointConfiguration.EndpointName, loggingEvent.Level.ToString(), loggingEvent.RenderedMessage);
+            }
+
+        }
+
+        ScenarioContext context;
+        readonly EndpointConfiguration endpointConfiguration;
+    }
+}
