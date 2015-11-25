a0851e5151fe17002c64528f7fe43fca04deb7ab
Tomasz Masternak <tomasz.masternak@particular.net>
2015-11-25 14:55:21 +0100

    fixing inspection error

--- src/NServiceBus.SqlServer/Guard/Resharper.Annotations.g.cs
+++ src/NServiceBus.SqlServer/Guard/Resharper.Annotations.g.cs
@@ -13,8 +13,6 @@ using System.Diagnostics;
 
 namespace JetBrains.Annotations
 {
-    using System.Diagnostics.Contracts;
-
     /// <summary>
     /// Indicates that the function argument should be string literal and match one
     /// of the parameters of the caller function. For example, ReSharper annotates
