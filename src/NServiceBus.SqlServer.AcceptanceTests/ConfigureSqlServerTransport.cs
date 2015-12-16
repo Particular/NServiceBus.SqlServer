452dbc4aa78b35c7b5f97e27226e49c437150acc
Weronika Łabaj <weronika.labaj@particular.net>
2015-12-16 12:14:18 +0100

    Delete queues after tests.

--- src/NServiceBus.SqlServer.AcceptanceTests/ConfigureSqlServerTransport.cs
+++ src/NServiceBus.SqlServer.AcceptanceTests/ConfigureSqlServerTransport.cs
@@ -1,22 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Transports;
 
 public class ConfigureSqlServerTransport : IConfigureTestExecution
 {
     BusConfiguration busConfiguration;
+    string connectionString;
 
     public Task Configure(BusConfiguration configuration, IDictionary<string, string> settings)
     {
         busConfiguration = configuration;
-        configuration.UseTransport<SqlServerTransport>().ConnectionString(settings["Transport.ConnectionString"]);
+        connectionString = settings["Transport.ConnectionString"];
+        configuration.UseTransport<SqlServerTransport>().ConnectionString(connectionString);
         return Task.FromResult(0);
     }
 
     public Task Cleanup()
     {
-        //TODO: add logic for removing queues used in tests
+        var bindings = busConfiguration.GetSettings().Get<QueueBindings>();
+        var queueNames = new List<string>();
+
+        using (var conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            var qn = bindings.ReceivingAddresses.ToList().ToList();
+            qn.ForEach(n =>
+            {
+                var nameParts = n.Split('@');
+                if (nameParts.Length == 2)
+                    queueNames.Add($"[{nameParts[1]}].[{nameParts[0]}]");
+                else
+                    queueNames.Add(n);
+            });
+            foreach (var queue in queueNames)
+            {
+                using (var comm = conn.CreateCommand())
+                {
+                    comm.CommandText = $"IF OBJECT_ID('{queue}', 'U') IS NOT NULL DROP TABLE {queue}";
+                    comm.ExecuteNonQuery();
+                }
+            }
+        }
 
         return Task.FromResult(0);
     }
