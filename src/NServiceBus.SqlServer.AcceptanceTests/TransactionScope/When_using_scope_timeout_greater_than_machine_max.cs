cdf871f9375cbc8d1e268e1c512c73c19bd57f69
Marcin Hoppe <marcin.hoppe@particular.net>
2016-01-11 13:43:01 +0100

    Configure tx scope options on SQL Server transport level

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/TransactionScope/When_using_scope_timeout_greater_than_machine_max.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.SqlServer.AcceptanceTests.TransactionScope
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Transports.SQLServer;
+    using NUnit.Framework;
+
+    public class When_using_scope_timeout_greater_than_machine_max : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_throw()
+        {
+            var exception = Assert.Throws<AggregateException>(async () =>
+            {
+                await Scenario.Define<Context>()
+                    .WithEndpoint<Endpoint>()
+                    .Run();
+            });
+
+            Assert.That(exception.InnerException.InnerException.Message.Contains("Timeout requested is longer than the maximum value for this machine"));
+        }
+
+        private class Context : ScenarioContext
+        {
+        }
+
+        private class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(busConfiguration =>
+                {
+                    busConfiguration.UseTransport<SqlServerTransport>()
+                        .Transactions(TransportTransactionMode.TransactionScope)
+                        .TransactionScopeOptions(timeout: TimeSpan.FromHours(1));
+                });
+            }
+        }
+    }
+}
