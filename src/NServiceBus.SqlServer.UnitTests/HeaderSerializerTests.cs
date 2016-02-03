183cf2c5c4bf3c9905ab28758c05ed2d6a9f6f30
SzymonPobiega <szymon.pobiega@gmail.com>
2016-02-03 09:31:36 +0100

    Added backwards compat test.

--- src/NServiceBus.SqlServer.UnitTests/HeaderSerializerTests.cs
+++ src/NServiceBus.SqlServer.UnitTests/HeaderSerializerTests.cs
@@ -1,26 +1,49 @@
 namespace NServiceBus.SqlServer.UnitTests
 {
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Runtime.Serialization.Formatters;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
     using NServiceBus.Transports.SQLServer;
     using NUnit.Framework;
 
     [TestFixture]
     public class HeaderSerializerTests
     {
-        [Test]
-        public void Do()
+        static readonly Dictionary<string, string> Expected = new Dictionary<string, string>
         {
-            var headers = new Dictionary<string, string>
+            { "SimpleKey", "SimpleValue"},
+            { "lowercaseKey", "lowercaseValue"},
+            { "qote\"ke'y", "quote\"valu'e"},
+            { "paren}key{}", "parent}value{}"},
+        };
+
+        [Test]
+        public void Roudtrip_test_various_case()
+        {
+            var headersDeserialized = HeaderSerializer.Deserialize(HeaderSerializer.Serialialize(Expected));
+            CollectionAssert.AreEquivalent(Expected, headersDeserialized);
+        }
+
+        [Test]
+        public void Test_compatibility_with_Newtonsoft()
+        {
+            var serializerSettings = new JsonSerializerSettings
             {
-                { "SimpleKey", "SimpleValue"},
-                { "lowercaseKey", "lowercaseValue"},
-                { "qote\"ke'y", "quote\"valu'e"},
-                { "paren}key{}", "parent}value{}"},
+                TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple,
+                TypeNameHandling = TypeNameHandling.Auto,
+                Converters =
+                {
+                    new IsoDateTimeConverter
+                    {
+                        DateTimeStyles = DateTimeStyles.RoundtripKind
+                    }
+                }
             };
 
-            var headersDeserialized = HeaderSerializer.Deserialize(HeaderSerializer.Serialialize(headers));
-
-            CollectionAssert.AreEquivalent(headers, headersDeserialized);
+            var headersDeserialized = HeaderSerializer.Deserialize(JsonConvert.SerializeObject(Expected, Formatting.None, serializerSettings));
+            CollectionAssert.AreEquivalent(Expected, headersDeserialized);
         }
     }
 }
