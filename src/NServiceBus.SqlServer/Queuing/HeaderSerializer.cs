2c5a50ed896f0f8fa467970ba7f1b5bf1915c6ce
SzymonPobiega <szymon.pobiega@gmail.com>
2016-02-03 15:28:15 +0100

    Using ilmerged json.net for headers.

--- src/NServiceBus.SqlServer/Queuing/HeaderSerializer.cs
+++ src/NServiceBus.SqlServer/Queuing/HeaderSerializer.cs
@@ -1,50 +1,34 @@
 namespace NServiceBus.Transports.SQLServer
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text.RegularExpressions;
+    using System.Globalization;
+    using System.Runtime.Serialization.Formatters;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
 
     static class HeaderSerializer
     {
-        static readonly Regex HeaderRegex = new Regex("\"(([^\"]|\\\\\")*)\"\\s*:\\s*(\"((([^\"]|\\\\\")*)\")|(null))\\s*(}|,)", RegexOptions.Compiled);
+        static JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+        {
+            TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple,
+            TypeNameHandling = TypeNameHandling.Auto,
+            Converters =
+            {
+                new IsoDateTimeConverter
+                {
+                    DateTimeStyles = DateTimeStyles.RoundtripKind
+                }
+            }
+        };
 
         internal static string Serialialize(Dictionary<string, string> headers)
         {
-            var props = headers.Select(kvp =>
-            {
-                var value = kvp.Value != null
-                    ? $"\"{Escape(kvp.Value)}\""
-                    : "null";
-
-                return $"\"{Escape(kvp.Key)}\":{value}";
-            });
-
-            return "{" + string.Join(", ", props) + "}";
+            return JsonConvert.SerializeObject(headers, Formatting.None, serializerSettings);
         }
-
-        static string Escape(string value)
-        {
-            return value.Replace("\"", "\\\"");
-        }
-
-        static string Unescape(string value)
-        {
-            return value.Replace("\\\"", "\"");
-        }
-
+        
         internal static Dictionary<string, string> Deserialize(string headerString)
         {
-            var match = HeaderRegex.Match(headerString);
-            var result = new Dictionary<string, string>();
-            while (match.Success)
-            {
-                var key = Unescape(match.Groups[1].Value);
-                var value = Unescape(match.Groups[5].Value);
-                result[key] = "null".Equals(match.Groups[7].Value, StringComparison.OrdinalIgnoreCase) ? null : value;
-                match = match.NextMatch();
-            }
-            return result;
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(headerString, serializerSettings);
         } 
     }
 }
