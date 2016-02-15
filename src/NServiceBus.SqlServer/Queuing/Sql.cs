7e72bd12b358e1bea9a73fb955f78a2c2f39a757
Marcin Hoppe <marcin.hoppe@particular.net>
2016-02-15 11:50:01 +0100

    Add index on the Expires column to optimize purging expires messages

--- src/NServiceBus.SqlServer/Queuing/Sql.cs
+++ src/NServiceBus.SqlServer/Queuing/Sql.cs
@@ -39,6 +39,11 @@ namespace NServiceBus.Transports.SQLServer
                         (
 	                        [RowVersion] ASC
                         )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+
+                        CREATE NONCLUSTERED INDEX [Index_Expires] ON [{0}].[{1}]
+                        (
+	                        [Expires] ASC
+                        )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
                     END
     
                     EXEC sp_releaseapplock @Resource = '{0}_{1}_lock'
