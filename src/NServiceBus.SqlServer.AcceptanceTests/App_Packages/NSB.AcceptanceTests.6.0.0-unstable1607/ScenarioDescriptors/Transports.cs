d38fc6730121655e39962a11c1040311d2161de2
Daniel Marbach <daniel.marbach@particular.net>
2016-02-29 10:59:53 +0100

    Changed ATTs infrastructure to newest ConfigureEndpointTestExecution

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/App_Packages/NSB.AcceptanceTests.6.0.0-unstable1607/ScenarioDescriptors/Transports.cs
@@ -0,0 +1,76 @@
+﻿namespace NServiceBus.AcceptanceTests.ScenarioDescriptors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NServiceBus.AcceptanceTesting.Support;
+    using NServiceBus.Transports;
+
+    public static class Transports
+    {
+        internal static IEnumerable<RunDescriptor> AllAvailable
+        {
+            get
+            {
+                foreach (var transportDefinitionType in foundDefinitions.Value)
+                {
+                    var key = transportDefinitionType.Name;
+
+                    var runDescriptor = new RunDescriptor(key);
+                    runDescriptor.Settings.Set("Transport", transportDefinitionType);
+
+                    var connectionString = Environment.GetEnvironmentVariable(key + ".ConnectionString");
+
+                    if (string.IsNullOrEmpty(connectionString) && DefaultConnectionStrings.ContainsKey(key))
+                    {
+                        connectionString = DefaultConnectionStrings[key];
+                    }
+
+
+                    if (!string.IsNullOrEmpty(connectionString))
+                    {
+                        runDescriptor.Settings.Set("Transport.ConnectionString", connectionString);
+                        yield return runDescriptor;
+                    }
+                }
+            }
+        }
+
+        public static RunDescriptor Default
+        {
+            get
+            {
+                var specificTransport = EnvironmentHelper.GetEnvironmentVariable("Transport.UseSpecific");
+
+                var runDescriptors = AllAvailable;
+                if (!string.IsNullOrEmpty(specificTransport))
+                {
+                    return runDescriptors.Single(r => r.Key == specificTransport);
+                }
+
+                var transportsOtherThanMsmq = runDescriptors.Where(t => t != Msmq);
+
+                if (transportsOtherThanMsmq.Count() == 1)
+                {
+                    return transportsOtherThanMsmq.First();
+                }
+
+                return Msmq;
+            }
+        }
+
+        public static RunDescriptor Msmq
+        {
+            get { return AllAvailable.SingleOrDefault(r => r.Key == "MsmqTransport"); }
+        }
+
+        static Lazy<List<Type>> foundDefinitions = new Lazy<List<Type>>(() => TypeScanner.GetAllTypesAssignableTo<TransportDefinition>().ToList());
+
+        static Dictionary<string, string> DefaultConnectionStrings = new Dictionary<string, string>
+        {
+            {"RabbitMQTransport", "host=localhost"},
+            {"SqlServerTransport", @"Server=localhost\sqlexpress;Database=nservicebus;Trusted_Connection=True;"},
+            {"MsmqTransport", @"cacheSendConnection=false;journal=false;"}
+        };
+    }
+}
---
+++ src/NServiceBus.SqlServer.AcceptanceTests/ConfigureEndpointSqlServerTransport.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Transports;
+
+public class ConfigureScenariosForSqlServerTransport : IConfigureSupportedScenariosForTestExecution
+{
+    public IEnumerable<Type> UnsupportedScenarioDescriptorTypes { get; } = new[]
+    {
+        typeof(AllTransportsWithCentralizedPubSubSupport)
+    };
+}
+
+public class ConfigureEndpointSqlServerTransport : IConfigureEndpointTestExecution
+{
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings)
+    {
+        this.configuration = configuration;
+        connectionString = settings.Get<string>("Transport.ConnectionString");
+        configuration.UseTransport<SqlServerTransport>().ConnectionString(connectionString);
+        return Task.FromResult(0);
+    }
+
+    public Task Cleanup()
+    {
+        var bindings = configuration.GetSettings().Get<QueueBindings>();
+        var queueNames = new List<string>();
+
+        using (var conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            var qn = bindings.ReceivingAddresses.ToList().ToList();
+            qn.ForEach(n =>
+            {
+                var nameParts = n.Split('@');
+                if (nameParts.Length == 2)
+                {
+                    queueNames.Add($"[{nameParts[1]}].[{nameParts[0]}]");
+                }
+                else
+                {
+                    queueNames.Add(n);
+                }
+            });
+            foreach (var queue in queueNames)
+            {
+                using (var comm = conn.CreateCommand())
+                {
+                    comm.CommandText = $"IF OBJECT_ID('{queue}', 'U') IS NOT NULL DROP TABLE {queue}";
+                    comm.ExecuteNonQuery();
+                }
+            }
+        }
+
+        return Task.FromResult(0);
+    }
+
+    EndpointConfiguration configuration;
+    string connectionString;
+}
