d761c67ac2f35bce323642c36114a15ffd64d646
Daniel Marbach <daniel.marbach@particular.net>
2016-05-11 12:52:39 +0200

    Opt out from waiting for DONE_IN_PROC messages by setting NO COUNT ON for statements that don't need it

--- src/NServiceBus.SqlServer/Queuing/Sql.cs
+++ src/NServiceBus.SqlServer/Queuing/Sql.cs
@@ -5,13 +5,27 @@ namespace NServiceBus.Transport.SQLServer
         internal const string PurgeText = "DELETE FROM {0}.{1}";
 
         internal const string SendText =
-            @"INSERT INTO {0}.{1} ([Id],[CorrelationId],[ReplyToAddress],[Recoverable],[Expires],[Headers],[Body])
-                                    VALUES (@Id,@CorrelationId,@ReplyToAddress,@Recoverable,CASE WHEN @TimeToBeReceivedMs IS NOT NULL THEN DATEADD(ms, @TimeToBeReceivedMs, GETUTCDATE()) END,@Headers,@Body)";
+            @"
+              DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
+              IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON'
+              SET NOCOUNT ON;
 
-        internal const string ReceiveText =
-            @"WITH message AS (SELECT TOP(1) * FROM {0}.{1} WITH (UPDLOCK, READPAST, ROWLOCK) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE() ORDER BY [RowVersion])
-			DELETE FROM message
-			OUTPUT deleted.Id, deleted.CorrelationId, deleted.ReplyToAddress, deleted.Recoverable, deleted.Headers, deleted.Body;";
+              INSERT INTO {0}.{1} ([Id],[CorrelationId],[ReplyToAddress],[Recoverable],[Expires],[Headers],[Body])
+              VALUES (@Id,@CorrelationId,@ReplyToAddress,@Recoverable,CASE WHEN @TimeToBeReceivedMs IS NOT NULL THEN DATEADD(ms, @TimeToBeReceivedMs, GETUTCDATE()) END,@Headers,@Body);;
+
+              IF(@NOCOUNT = 'ON') SET NOCOUNT ON;
+              IF(@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
+
+        internal const string ReceiveText = @"
+            DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
+            IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON';
+            SET NOCOUNT ON;
+
+            WITH message AS (SELECT TOP(1) * FROM {0}.{1} WITH (UPDLOCK, READPAST, ROWLOCK) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE() ORDER BY [RowVersion])
+            DELETE FROM message
+            OUTPUT deleted.Id, deleted.CorrelationId, deleted.ReplyToAddress, deleted.Recoverable, deleted.Headers, deleted.Body;
+            IF(@NOCOUNT = 'ON') SET NOCOUNT ON;
+            IF(@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
 
         internal const string PeekText = "SELECT count(*) Id FROM {0}.{1} WITH (READPAST) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE();";
 
@@ -22,24 +36,24 @@ namespace NServiceBus.Transport.SQLServer
                     IF NOT  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
                     BEGIN
                         CREATE TABLE [{0}].[{1}](
-	                        [Id] [uniqueidentifier] NOT NULL,
-	                        [CorrelationId] [varchar](255) NULL,
-	                        [ReplyToAddress] [varchar](255) NULL,
-	                        [Recoverable] [bit] NOT NULL,
-	                        [Expires] [datetime] NULL,
-	                        [Headers] [varchar](max) NOT NULL,
-	                        [Body] [varbinary](max) NULL,
-	                        [RowVersion] [bigint] IDENTITY(1,1) NOT NULL
+                            [Id] [uniqueidentifier] NOT NULL,
+                            [CorrelationId] [varchar](255) NULL,
+                            [ReplyToAddress] [varchar](255) NULL,
+                            [Recoverable] [bit] NOT NULL,
+                            [Expires] [datetime] NULL,
+                            [Headers] [varchar](max) NOT NULL,
+                            [Body] [varbinary](max) NULL,
+                            [RowVersion] [bigint] IDENTITY(1,1) NOT NULL
                         ) ON [PRIMARY];
 
                         CREATE CLUSTERED INDEX [Index_RowVersion] ON [{0}].[{1}]
                         (
-	                        [RowVersion] ASC
+                            [RowVersion] ASC
                         )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 
                         CREATE NONCLUSTERED INDEX [Index_Expires] ON [{0}].[{1}]
                         (
-	                        [Expires] ASC
+                            [Expires] ASC
                         )
                         INCLUDE
                         (
