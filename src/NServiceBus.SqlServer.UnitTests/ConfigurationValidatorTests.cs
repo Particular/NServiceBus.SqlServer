8f6c6aba68ae31ef2dabc9635929e50a4da096dc
Simon Cropp <simon.cropp@gmail.com>
2016-05-25 17:48:24 +1000

    update refs
    -core to v6 beta3
    -nunit 3
    -Janitor

--- src/NServiceBus.SqlServer.UnitTests/ConfigurationValidatorTests.cs
+++ src/NServiceBus.SqlServer.UnitTests/ConfigurationValidatorTests.cs
@@ -22,50 +22,50 @@
         [Test]
         public void Validation_passes_when_there_is_single_transport_connections_string_without_schema()
         {
-            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport", "Soruce = xxx"));
+            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport", "Source = xxx"));
 
             Assert.IsTrue(result.Success);
-            Assert.IsNullOrEmpty(result.Message);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Message));
         }
 
         [Test]
         public void Validation_fails_when_transport_connection_string_has_schema_override()
         {
-            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport", "Soruce = xxx; Queue schema= yyy"));
+            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport", "Source = xxx; Queue schema= yyy"));
 
             Assert.IsFalse(result.Success);
-            Assert.IsNotNullOrEmpty(result.Message);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Message));
         }
 
         [Test]
         public void Validation_fails_when_there_is_more_than_one_transport_connection_string()
         {
             var result = Validate(
-                new ConnectionStringSettings("NServiceBus/Transport", "Soruce = xxx;"),
-                new ConnectionStringSettings("NServiceBus/Transport/Endpoint1", "Soruce = xxx;"));
+                new ConnectionStringSettings("NServiceBus/Transport", "Source = xxx;"),
+                new ConnectionStringSettings("NServiceBus/Transport/Endpoint1", "Source = xxx;"));
 
             Assert.IsFalse(result.Success);
-            Assert.IsNotNullOrEmpty(result.Message);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Message));
         }
 
         [Test]
         public void Validation_passes_when_there_are_more_connection_strings_but_only_one_is_transport()
         {
             var result = Validate(
-                new ConnectionStringSettings("NServiceBus/Transport", "Soruce = xxx;"),
-                new ConnectionStringSettings("OtherConnectionString", "Soruce = xxx;"));
+                new ConnectionStringSettings("NServiceBus/Transport", "Source = xxx;"),
+                new ConnectionStringSettings("OtherConnectionString", "Source = xxx;"));
 
             Assert.IsTrue(result.Success);
-            Assert.IsNullOrEmpty(result.Message);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Message));
         }
 
         [Test]
         public void Validation_fails_when_only_single_endpoint_specific_transport_connection_string_exists()
         {
-            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport/Endpoint1", "Soruce = xxx;"));
+            var result = Validate(new ConnectionStringSettings("NServiceBus/Transport/Endpoint1", "Source = xxx;"));
 
             Assert.IsFalse(result.Success);
-            Assert.IsNotNullOrEmpty(result.Message);
+            Assert.IsFalse(string.IsNullOrEmpty(result.Message));
         }
 
         [Test]
@@ -74,7 +74,7 @@
             var result = Validate(new ConnectionStringSettings("NoTransport", "Soruce = xxx;"));
 
             Assert.IsTrue(result.Success);
-            Assert.IsNullOrEmpty(result.Message);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Message));
         }
 
         class Result
