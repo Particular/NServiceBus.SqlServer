6be49b1fbf3ec89c2898bf507907feaf6f53f01e
Ramon Smits <ramon.smits@gmail.com>
2016-06-08 14:12:44 +0200

    Grammar and magic number

--- src/NServiceBus.SqlServer/Receiving/MessagePump.cs
+++ src/NServiceBus.SqlServer/Receiving/MessagePump.cs
@@ -36,7 +36,7 @@
             {
                 var purgedRowsCount = await queuePurger.Purge(inputQueue).ConfigureAwait(false);
 
-                Logger.InfoFormat("{0} messages was purged from table {1}", purgedRowsCount, settings.InputQueue);
+                Logger.InfoFormat("{0:N} messages purged from table {1}", purgedRowsCount, settings.InputQueue);
             }
 
             await expiredMessagesPurger.Initialize(inputQueue).ConfigureAwait(false);
@@ -56,10 +56,11 @@
 
         public async Task Stop()
         {
+            const int timeoutDurationInSeconds = 30;
             cancellationTokenSource.Cancel();
 
             // ReSharper disable once MethodSupportsCancellation
-            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(30));
+            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(timeoutDurationInSeconds));
             var allTasks = runningReceiveTasks.Values.Concat(new[]
             {
                 messagePumpTask,
@@ -69,7 +70,7 @@
 
             if (finishedTask.Equals(timeoutTask))
             {
-                Logger.Error("The message pump failed to stop with in the time allowed(30s)");
+                Logger.ErrorFormat("The message pump failed to stop within the time allowed ({0}s)", timeoutDurationInSeconds);
             }
 
             concurrencyLimiter.Dispose();
