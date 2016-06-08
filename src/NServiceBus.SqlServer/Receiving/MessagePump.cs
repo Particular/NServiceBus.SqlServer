cf5721203eae48110827625e2d7053d895797cd9
Ramon Smits <ramon.smits@gmail.com>
2016-06-08 20:41:11 +0200

    Grammar and magic number (#254)
    * Grammar and magic number
    
    * Term 'table' renamed to 'queue' in logging

--- src/NServiceBus.SqlServer/Receiving/MessagePump.cs
+++ src/NServiceBus.SqlServer/Receiving/MessagePump.cs
@@ -36,7 +36,7 @@
             {
                 var purgedRowsCount = await queuePurger.Purge(inputQueue).ConfigureAwait(false);
 
-                Logger.InfoFormat("{0} messages was purged from table {1}", purgedRowsCount, settings.InputQueue);
+                Logger.InfoFormat("{0:N} messages purged from queue {1}", purgedRowsCount, settings.InputQueue);
             }
 
             await expiredMessagesPurger.Initialize(inputQueue).ConfigureAwait(false);
@@ -56,10 +56,11 @@
 
         public async Task Stop()
         {
+            const int timeoutDurationInSeconds = 30;
             cancellationTokenSource.Cancel();
 
             // ReSharper disable once MethodSupportsCancellation
-            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(30));
+            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(timeoutDurationInSeconds));
             var allTasks = runningReceiveTasks.Values.Concat(new[]
             {
                 messagePumpTask,
@@ -69,7 +70,7 @@
 
             if (finishedTask.Equals(timeoutTask))
             {
-                Logger.Error("The message pump failed to stop with in the time allowed(30s)");
+                Logger.ErrorFormat("The message pump failed to stop within the time allowed ({0}s)", timeoutDurationInSeconds);
             }
 
             concurrencyLimiter.Dispose();
@@ -171,7 +172,7 @@
                 {
                     await expiredMessagesPurger.Purge(inputQueue, cancellationToken).ConfigureAwait(false);
 
-                    Logger.DebugFormat("Scheduling next expired message purge task for table {0} in {1}", inputQueue, expiredMessagesPurger.PurgeTaskDelay);
+                    Logger.DebugFormat("Scheduling next expired message purge task for queue {0} in {1}", inputQueue, expiredMessagesPurger.PurgeTaskDelay);
                     await Task.Delay(expiredMessagesPurger.PurgeTaskDelay, cancellationToken).ConfigureAwait(false);
                 }
                 catch (OperationCanceledException)
