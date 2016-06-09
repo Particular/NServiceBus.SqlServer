7e1d3672c9428ead37cd53710f6b03bf1890f44c
Marcin Hoppe <marcin.hoppe@particular.net>
2016-06-09 21:23:01 +0200

    Refactor unit test for better readability

--- src/NServiceBus.SqlServer.UnitTests/TableBasedQueueTests.cs
+++ src/NServiceBus.SqlServer.UnitTests/TableBasedQueueTests.cs
@@ -9,7 +9,7 @@
         public void Table_name_and_schema_should_be_quoted()
         {
             Assert.AreEqual("[nsb].[MyEndpoint]", new TableBasedQueue("MyEndpoint", "nsb").ToString());
-            Assert.AreNotEqual("[nsb].[MyEndoint]SomeOtherData]", new TableBasedQueue("MyEndoint]SomeOtherData", "nsb").ToString());
+            Assert.AreEqual("[nsb].[MyEndoint]]; SOME OTHER SQL;--]", new TableBasedQueue("MyEndoint]; SOME OTHER SQL;--", "nsb").ToString());
         }
     }
 }
