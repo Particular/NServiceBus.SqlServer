ee8a63eb273f2701df39098728e3f0637f3260d1
Marcin Hoppe <marcin.hoppe@particular.net>
2016-11-09 14:48:38 +0100

    Add test for pub/sub with multi-schema
    Revert DotSettings change

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/MultiSchema/When_custom_schema_configured_for_publisher.cs
@@ -0,0 +1,77 @@
+﻿namespace NServiceBus.SqlServer.AcceptanceTests.MultiSchema
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.AcceptanceTests.Routing;
+    using NUnit.Framework;
+    using Transport.SQLServer;
+
+    public class When_custom_schema_configured_for_publisher : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_receive_event()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<Publisher>(b => b.When(c => c.Subscribed, session => session.Publish(new Event())))
+                .WithEndpoint<Subscriber>()
+                .Done(c => c.EventReceived)
+                .Run();
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool Subscribed { get; set; }
+            public bool EventReceived { get; set; }
+        }
+
+        class Publisher : EndpointConfigurationBuilder
+        {
+            public Publisher()
+            {
+                EndpointSetup<DefaultPublisher>(b =>
+                {
+                    b.UseTransport<SqlServerTransport>()
+                        .DefaultSchema("sender");
+
+                    b.OnEndpointSubscribed<Context>((args, context) => { context.Subscribed = true; });
+                });
+            }
+        }
+
+        class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(b =>
+                {
+                    var publisherEndpoint = Conventions.EndpointNamingConvention(typeof(Publisher));
+
+                    b.UseTransport<SqlServerTransport>()
+                        .DefaultSchema("receiver")
+                        .UseSchemaForEndpoint(publisherEndpoint, "sender")
+                        .Routing().RegisterPublisher(
+                            eventType: typeof(Event),
+                            publisherEndpoint: publisherEndpoint);
+                });
+            }
+
+            class EventHandler : IHandleMessages<Event>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(Event message, IMessageHandlerContext context)
+                {
+                    Context.EventReceived = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        class Event : IEvent
+        {
+        }
+    }
+}
