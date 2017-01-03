879b9e8ad3fad129cd9d0703be72e274b7942e80
SzymonPobiega <szymon.pobiega@gmail.com>
2017-01-03 15:27:45 +0100

    Move TryReceive up the inheritance chain.

--- src/NServiceBus.SqlServer/Receiving/ReceiveWithNoTransaction.cs
+++ src/NServiceBus.SqlServer/Receiving/ReceiveWithNoTransaction.cs
@@ -15,24 +15,14 @@ namespace NServiceBus.Transport.SQLServer
         {
             using (var connection = await connectionFactory.OpenNewConnection().ConfigureAwait(false))
             {
-                var readResult = await InputQueue.TryReceive(connection, null).ConfigureAwait(false);
-
-                if (readResult.IsPoison)
+                var message = await TryReceive(connection, null, receiveCancellationTokenSource).ConfigureAwait(false);
+                if (message == null)
                 {
-                    await ErrorQueue.DeadLetter(readResult.PoisonMessage, connection, null).ConfigureAwait(false);
-                    return;
-                }
-
-                if (!readResult.Successful)
-                {
-                    receiveCancellationTokenSource.Cancel();
                     return;
                 }
 
                 var transportTransaction = new TransportTransaction();
                 transportTransaction.Set(connection);
-
-                var message = readResult.Message;
 
                 try
                 {
