61205bcbd5fa7568a122b6d73f28caa3335b0f22
SzymonPobiega <szymon.pobiega@gmail.com>
2017-02-27 07:44:53 +0100

    Fixed unicode in clean up script.

--- src/Scripts/Reset-Database.sql
+++ src/Scripts/Reset-Database.sql
@@ -14,17 +14,20 @@ END
 GO
 
 /* Drop all views */
-DECLARE @name VARCHAR(128)
-DECLARE @SQL VARCHAR(254)
+DECLARE @name NVARCHAR(128)
+DECLARE @SQL NVARCHAR(254)
+DECLARE @schema NVARCHAR(128)
 
-SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'V' AND category = 0 ORDER BY [name])
+SELECT TOP 1 @name = sys.objects.name, @schema = sys.schemas.name FROM sys.objects INNER JOIN sys.schemas ON sys.objects.schema_id = sys.schemas.schema_id WHERE [type] = 'V' ORDER BY sys.objects.name
+--SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'V' AND category = 0 ORDER BY [name])
 
 WHILE @name IS NOT NULL
 BEGIN
-    SELECT @SQL = 'DROP VIEW [dbo].[' + RTRIM(@name) +']'
+    SELECT @SQL = 'DROP VIEW [' + @schema + '].[' + RTRIM(@name) +']'
     EXEC (@SQL)
-    PRINT 'Dropped View: ' + @name
-    SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'V' AND category = 0 AND [name] > @name ORDER BY [name])
+    PRINT 'Dropped View: ' + @schema + '.' + @name
+	SELECT  @name = NULL
+	SELECT TOP 1 @name = sys.objects.name, @schema = sys.schemas.name FROM sys.objects INNER JOIN sys.schemas ON sys.objects.schema_id = sys.schemas.schema_id WHERE [type] = 'V' ORDER BY sys.objects.name    
 END
 GO
 
@@ -44,51 +47,45 @@ END
 GO
 
 /* Drop all Foreign Key constraints */
-DECLARE @name VARCHAR(128)
-DECLARE @constraint VARCHAR(254)
-DECLARE @SQL VARCHAR(254)
+DECLARE @name NVARCHAR(128)
+DECLARE @schema NVARCHAR(128)
+DECLARE @constraint NVARCHAR(254)
+DECLARE @SQL NVARCHAR(254)
 
-SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME)
+SELECT TOP 1 @name = TABLE_NAME, @schema = CONSTRAINT_SCHEMA, @constraint = CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME
 
 WHILE @name is not null
-BEGIN
-    SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
-    WHILE @constraint IS NOT NULL
-    BEGIN
-        SELECT @SQL = 'ALTER TABLE [dbo].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint) +']'
-        EXEC (@SQL)
-        PRINT 'Dropped FK Constraint: ' + @constraint + ' on ' + @name
-        SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_NAME <> @constraint AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
-    END
-SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME)
+BEGIN    
+    SELECT @SQL = 'ALTER TABLE [' + @schema + '].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint) +']'
+    EXEC (@SQL)
+    PRINT 'Dropped FK Constraint: ' + @constraint + ' on ' + @schema + '.' + @name
+	SELECT  @name = NULL
+	SELECT TOP 1 @name = TABLE_NAME, @schema = CONSTRAINT_SCHEMA, @constraint = CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME
 END
 GO
 
 /* Drop all Primary Key constraints */
-DECLARE @name VARCHAR(128)
-DECLARE @constraint VARCHAR(254)
-DECLARE @SQL VARCHAR(254)
+DECLARE @name NVARCHAR(128)
+DECLARE @schema NVARCHAR(128)
+DECLARE @constraint NVARCHAR(254)
+DECLARE @SQL NVARCHAR(254)
 
-SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME)
+SELECT TOP 1 @name = TABLE_NAME, @schema = CONSTRAINT_SCHEMA, @constraint = CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME
 
 WHILE @name IS NOT NULL
-BEGIN
-    SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
-    WHILE @constraint is not null
-    BEGIN
-        SELECT @SQL = 'ALTER TABLE [dbo].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint)+']'
-        EXEC (@SQL)
-        PRINT 'Dropped PK Constraint: ' + @constraint + ' on ' + @name
-        SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' AND CONSTRAINT_NAME <> @constraint AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
-    END
-SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME)
+BEGIN    
+    SELECT @SQL = 'ALTER TABLE [' + @schema + '].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint)+']'
+    EXEC (@SQL)
+    PRINT 'Dropped PK Constraint: ' + @constraint + ' on ' + @schema + '.' + @name
+    SELECT  @name = NULL
+	SELECT TOP 1 @name = TABLE_NAME, @schema = CONSTRAINT_SCHEMA, @constraint = CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME	
 END
 GO
 
 /* Drop all tables */
-DECLARE @name VARCHAR(128)
-DECLARE @schema VARCHAR(128)
-DECLARE @SQL VARCHAR(254)
+DECLARE @name NVARCHAR(128)
+DECLARE @schema NVARCHAR(128)
+DECLARE @SQL NVARCHAR(254)
 
 SELECT TOP 1 @name = sys.objects.name, @schema = sys.schemas.name FROM sys.objects INNER JOIN sys.schemas ON sys.objects.schema_id = sys.schemas.schema_id WHERE [type] = 'U' ORDER BY sys.objects.name
 
