b6fa81dc0f3a33e157b854c320bb4f0a32f29452
Simon Cropp <simon.cropp@gmail.com>
2017-03-09 10:26:41 +1100

    remove redundant [ ]

--- src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
+++ src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
@@ -17,7 +17,7 @@ DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
 IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON'
 SET NOCOUNT ON;
 
-INSERT INTO {0}.{1} ([Id], [CorrelationId], [ReplyToAddress], [Recoverable], [Expires], [Headers], [Body])
+INSERT INTO {0}.{1} (Id, CorrelationId, ReplyToAddress, Recoverable, Expires, Headers, Body)
 VALUES (@Id, @CorrelationId, @ReplyToAddress, @Recoverable, CASE WHEN @TimeToBeReceivedMs IS NOT NULL THEN DATEADD(ms, @TimeToBeReceivedMs, GETUTCDATE()) END, @Headers, @Body);
 
 IF (@NOCOUNT = 'ON') SET NOCOUNT ON;
@@ -28,54 +28,54 @@ DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
 IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON';
 SET NOCOUNT ON;
 
-WITH message AS (SELECT TOP(1) * FROM {0}.{1} WITH (UPDLOCK, READPAST, ROWLOCK) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE() ORDER BY [RowVersion])
+WITH message AS (SELECT TOP(1) * FROM {0}.{1} WITH (UPDLOCK, READPAST, ROWLOCK) WHERE Expires IS NULL OR Expires > GETUTCDATE() ORDER BY RowVersion)
 DELETE FROM message
 OUTPUT deleted.Id, deleted.CorrelationId, deleted.ReplyToAddress, deleted.Recoverable, deleted.Headers, deleted.Body;
 IF (@NOCOUNT = 'ON') SET NOCOUNT ON;
 IF (@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
 
-        public static readonly string PeekText = "SELECT count(*) Id FROM {0}.{1} WITH (READPAST) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE();";
+        public static readonly string PeekText = "SELECT count(*) Id FROM {0}.{1} WITH (READPAST) WHERE Expires IS NULL OR Expires > GETUTCDATE();";
 
         public static readonly string CreateQueueText = @"
 IF NOT  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
 BEGIN
     EXEC sp_getapplock @Resource = '{0}_{1}_lock', @LockMode = 'Exclusive'
 
-    IF NOT  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
+    IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
     BEGIN
         CREATE TABLE [{0}].[{1}](
-            [Id] [uniqueidentifier] NOT NULL,
-            [CorrelationId] [varchar](255) NULL,
-            [ReplyToAddress] [varchar](255) NULL,
-            [Recoverable] [bit] NOT NULL,
-            [Expires] [datetime] NULL,
-            [Headers] [varchar](max) NOT NULL,
-            [Body] [varbinary](max) NULL,
-            [RowVersion] [bigint] IDENTITY(1,1) NOT NULL
-        ) ON [PRIMARY];
+            Id uniqueidentifier NOT NULL,
+            CorrelationId varchar(255) NULL,
+            ReplyToAddress varchar(255) NULL,
+            Recoverable bit NOT NULL,
+            Expires datetime NULL,
+            Headers varchar(max) NOT NULL,
+            Body varbinary(max) NULL,
+            RowVersion bigint IDENTITY(1,1) NOT NULL
+        ) ON PRIMARY;
 
-        CREATE CLUSTERED INDEX [Index_RowVersion] ON [{0}].[{1}]
+        CREATE CLUSTERED INDEX Index_RowVersion ON [{0}].[{1}]
         (
-            [RowVersion] ASC
-        ) ON [PRIMARY]
+            RowVersion ASC
+        ) ON PRIMARY
 
-        CREATE NONCLUSTERED INDEX [Index_Expires] ON [{0}].[{1}]
+        CREATE NONCLUSTERED INDEX Index_Expires ON [{0}].[{1}]
         (
-            [Expires] ASC
+            Expires ASC
         )
         INCLUDE
         (
-            [Id],
-            [RowVersion]
+            Id,
+            RowVersion
         )
     END
 
     EXEC sp_releaseapplock @Resource = '{0}_{1}_lock'
 END";
 
-        public static readonly string PurgeBatchOfExpiredMessagesText = "DELETE FROM {1}.{2} WHERE [RowVersion] IN (SELECT TOP ({0}) [RowVersion] FROM {1}.{2} WITH (NOLOCK) WHERE [Expires] < GETUTCDATE())";
+        public static readonly string PurgeBatchOfExpiredMessagesText = "DELETE FROM {1}.{2} WHERE RowVersion IN (SELECT TOP ({0}) RowVersion FROM {1}.{2} WITH (NOLOCK) WHERE Expires < GETUTCDATE())";
 
-        public static readonly string CheckIfExpiresIndexIsPresent = @"SELECT COUNT(*) FROM [sys].[indexes] WHERE [name] = 'Index_Expires' AND [object_id] = OBJECT_ID('{0}.{1}')";
+        public static readonly string CheckIfExpiresIndexIsPresent = @"SELECT COUNT(*) FROM sys.indexes WHERE name = 'Index_Expires' AND object_id = OBJECT_ID('{0}.{1}')";
         
     }
 }
