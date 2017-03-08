5e3543515a737cd492a25cba17696a51dc714236
Simon Cropp <simon.cropp@gmail.com>
2017-03-09 10:23:43 +1100

    Remove redundant create index defaults
    PAD_INDEX = OFF
    STATISTICS_NORECOMPUTE = OFF
    IGNORE_DUP_KEY = OFF
    DROP_EXISTING = OFF
    ONLINE = OFF
    ALLOW_ROW_LOCKS = ON
    ALLOW_PAGE_LOCKS = ON
    SORT_IN_TEMPDB = OFF

--- src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
+++ src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
@@ -57,7 +57,7 @@ BEGIN
         CREATE CLUSTERED INDEX [Index_RowVersion] ON [{0}].[{1}]
         (
             [RowVersion] ASC
-        ) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+        ) ON [PRIMARY]
 
         CREATE NONCLUSTERED INDEX [Index_Expires] ON [{0}].[{1}]
         (
@@ -68,7 +68,6 @@ BEGIN
             [Id],
             [RowVersion]
         )
-        WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
     END
 
     EXEC sp_releaseapplock @Resource = '{0}_{1}_lock'
