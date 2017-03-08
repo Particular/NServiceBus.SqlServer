ec0e85e4b414a839e855d921850dad26bec46657
Simon Cropp <simon.cropp@gmail.com>
2017-03-09 10:13:58 +1100

    clean up whitespace in sql statements

--- src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
+++ src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
@@ -17,11 +17,11 @@ DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
 IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON'
 SET NOCOUNT ON;
 
-INSERT INTO {0}.{1} ([Id],[CorrelationId],[ReplyToAddress],[Recoverable],[Expires],[Headers],[Body])
-VALUES (@Id,@CorrelationId,@ReplyToAddress,@Recoverable,CASE WHEN @TimeToBeReceivedMs IS NOT NULL THEN DATEADD(ms, @TimeToBeReceivedMs, GETUTCDATE()) END,@Headers,@Body);;
+INSERT INTO {0}.{1} ([Id], [CorrelationId], [ReplyToAddress], [Recoverable], [Expires], [Headers], [Body])
+VALUES (@Id, @CorrelationId, @ReplyToAddress, @Recoverable, CASE WHEN @TimeToBeReceivedMs IS NOT NULL THEN DATEADD(ms, @TimeToBeReceivedMs, GETUTCDATE()) END, @Headers, @Body);
 
-IF(@NOCOUNT = 'ON') SET NOCOUNT ON;
-IF(@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
+IF (@NOCOUNT = 'ON') SET NOCOUNT ON;
+IF (@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
 
         public static readonly string ReceiveText = @"
 DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
@@ -31,8 +31,8 @@ SET NOCOUNT ON;
 WITH message AS (SELECT TOP(1) * FROM {0}.{1} WITH (UPDLOCK, READPAST, ROWLOCK) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE() ORDER BY [RowVersion])
 DELETE FROM message
 OUTPUT deleted.Id, deleted.CorrelationId, deleted.ReplyToAddress, deleted.Recoverable, deleted.Headers, deleted.Body;
-IF(@NOCOUNT = 'ON') SET NOCOUNT ON;
-IF(@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
+IF (@NOCOUNT = 'ON') SET NOCOUNT ON;
+IF (@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
 
         public static readonly string PeekText = "SELECT count(*) Id FROM {0}.{1} WITH (READPAST) WHERE [Expires] IS NULL OR [Expires] > GETUTCDATE();";
 
@@ -57,7 +57,7 @@ BEGIN
         CREATE CLUSTERED INDEX [Index_RowVersion] ON [{0}].[{1}]
         (
             [RowVersion] ASC
-        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+        ) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 
         CREATE NONCLUSTERED INDEX [Index_Expires] ON [{0}].[{1}]
         (
