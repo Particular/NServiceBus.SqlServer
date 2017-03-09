37e99b4f2419e01c0ef8e60d8c99362ab8f9e59e
SzymonPobiega <szymon.pobiega@gmail.com>
2017-03-09 12:28:05 +0100

    Reduce nesting in create table.

--- src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
+++ src/NServiceBus.SqlServer/Queuing/SqlConstants.cs
@@ -66,49 +66,51 @@ WHERE Expires IS NULL
     OR Expires > GETUTCDATE();";
 
         public static readonly string CreateQueueText = @"
-IF NOT EXISTS (
+IF EXISTS (
     SELECT * 
     FROM sys.objects 
     WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') 
         AND type in (N'U'))
+RETURN
+
+EXEC sp_getapplock @Resource = '{0}_{1}_lock', @LockMode = 'Exclusive'
+
+IF EXISTS (
+    SELECT *
+    FROM sys.objects
+    WHERE object_id = OBJECT_ID(N'[{0}].[{1}]')
+        AND type in (N'U'))
 BEGIN
-    EXEC sp_getapplock @Resource = '{0}_{1}_lock', @LockMode = 'Exclusive'
-
-    IF NOT EXISTS (
-        SELECT *
-        FROM sys.objects
-        WHERE object_id = OBJECT_ID(N'[{0}].[{1}]')
-            AND type in (N'U'))
-    BEGIN
-        CREATE TABLE [{0}].[{1}](
-            Id uniqueidentifier NOT NULL,
-            CorrelationId varchar(255),
-            ReplyToAddress varchar(255),
-            Recoverable bit NOT NULL,
-            Expires datetime,
-            Headers varchar(max) NOT NULL,
-            Body varbinary(max),
-            RowVersion bigint IDENTITY(1,1) NOT NULL
-        ) ON [PRIMARY];
-
-        CREATE CLUSTERED INDEX Index_RowVersion ON [{0}].[{1}]
-        (
-            RowVersion ASC
-        ) ON [PRIMARY]
-
-        CREATE NONCLUSTERED INDEX Index_Expires ON [{0}].[{1}]
-        (
-            Expires ASC
-        )
-        INCLUDE
-        (
-            Id,
-            RowVersion
-        )
-    END
-
     EXEC sp_releaseapplock @Resource = '{0}_{1}_lock'
-END";
+    RETURN
+END
+
+CREATE TABLE [{0}].[{1}](
+    Id uniqueidentifier NOT NULL,
+    CorrelationId varchar(255),
+    ReplyToAddress varchar(255),
+    Recoverable bit NOT NULL,
+    Expires datetime,
+    Headers varchar(max) NOT NULL,
+    Body varbinary(max),
+    RowVersion bigint IDENTITY(1,1) NOT NULL
+) ON [PRIMARY];
+
+CREATE CLUSTERED INDEX Index_RowVersion ON [{0}].[{1}]
+(
+    RowVersion ASC
+) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX Index_Expires ON [{0}].[{1}]
+(
+    Expires ASC
+)
+INCLUDE
+(
+    Id,
+    RowVersion
+)
+";
 
         public static readonly string PurgeBatchOfExpiredMessagesText = @"
 DELETE FROM {1}.{2}
