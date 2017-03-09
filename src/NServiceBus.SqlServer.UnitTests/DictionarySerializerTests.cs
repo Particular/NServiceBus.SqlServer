943e806082be83d70463acfec671476d420b70d7
Simon Cropp <simon.cropp@gmail.com>
2017-03-10 09:40:59 +1100

    add DictionarySerializerTests

---
+++ src/NServiceBus.SqlServer.UnitTests/DictionarySerializerTests.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus.SqlServer.UnitTests
+{
+    using System.Collections.Generic;
+    using ApprovalTests;
+    using NUnit.Framework;
+    using Transport.SQLServer;
+
+    [TestFixture]
+    public class DictionarySerializerTests
+    {
+
+        [Test]
+        public void Can_round_trip()
+        {
+            var before = new Dictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"keyWithNull", null},
+                {"keyWithEmpty", ""}
+            };
+            var serialize = DictionarySerializer.Serialize(before);
+            Approvals.Verify(serialize);
+
+            var after = DictionarySerializer.DeSerialize(serialize);
+
+            AssertDictionariesAreTheSame(before, after);
+        }
+
+        void AssertDictionariesAreTheSame(Dictionary<string, string> before, Dictionary<string, string> after)
+        {
+            foreach (var beforeitem in before)
+            {
+                Assert.AreEqual(beforeitem.Value, after[beforeitem.Key]);
+            }
+        }
+    }
+}
