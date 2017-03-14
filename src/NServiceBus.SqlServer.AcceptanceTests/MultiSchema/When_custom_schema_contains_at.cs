af280c6ed0cd39ce09c868a40d6f51dd3294d1b5
SzymonPobiega <szymon.pobiega@gmail.com>
2017-03-14 12:53:18 +0100

    Added a test for @ in schema.

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/MultiSchema/When_custom_schema_contains_at.cs
@@ -0,0 +1,55 @@
+﻿namespace NServiceBus.SqlServer.AcceptanceTests.MultiSchema
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+    using Transport.SQLServer;
+
+    public class When_custom_schema_contains_at : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public Task Should_receive_message()
+        {
+            return Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(b => b.When((bus, c) => bus.SendLocal(new Message())))
+                .Done(c => c.MessageReceived)
+                .Repeat(r => r.For(NServiceBus.AcceptanceTests.ScenarioDescriptors.Transports.Default))
+                .Should(c => Assert.True(c.MessageReceived, "Message should be properly received"))
+                .Run();
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.UseTransport<SqlServerTransport>().DefaultSchema("db@");
+                });
+            }
+
+            class Handler : IHandleMessages<Message>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(Message message, IMessageHandlerContext context)
+                {
+                    Context.MessageReceived = true;
+
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class Message : IMessage
+        {
+        }
+    }
+}
