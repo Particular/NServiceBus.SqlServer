3bdbd99b34657c62540fa1c7468d9ceef722d060
SzymonPobiega <szymon.pobiega@gmail.com>
2017-06-27 14:33:59 +0200

    Inspections.

--- src/NServiceBus.SqlServer/Sending/MessageDispatcher.cs
+++ src/NServiceBus.SqlServer/Sending/MessageDispatcher.cs
@@ -5,7 +5,6 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Extensibility;
-    using Performance.TimeToBeReceived;
     using Transport;
 
     class MessageDispatcher : IDispatchMessages
