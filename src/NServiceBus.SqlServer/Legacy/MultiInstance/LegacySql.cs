17295307ce8ac3d712adc3771c5dd76d1e34e306
Simon Cropp <simon.cropp@gmail.com>
2017-07-06 23:53:52 +1000

    remove redundant index settings

--- src/NServiceBus.SqlServer/Legacy/MultiInstance/LegacySql.cs
+++ src/NServiceBus.SqlServer/Legacy/MultiInstance/LegacySql.cs
@@ -22,7 +22,7 @@ namespace NServiceBus.Transport.SQLServer
                         CREATE CLUSTERED INDEX [Index_RowVersion] ON {0}
                         (
                             [RowVersion]
-                        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+                        )
 
                         CREATE NONCLUSTERED INDEX [Index_Expires] ON {0}
                         (
@@ -33,7 +33,6 @@ namespace NServiceBus.Transport.SQLServer
                             [Id],
                             [RowVersion]
                         )
-                        WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
                     END
 
                     EXEC sp_releaseapplock @Resource = '{0}_lock'
