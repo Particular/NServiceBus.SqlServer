5d898544572f8701bd10995baa7004ce6a8cb64f
SzymonPobiega <szymon.pobiega@gmail.com>
2017-09-05 13:11:24 +0200

    AT queue cleanup refactoring

--- src/NServiceBus.SqlServer.AcceptanceTests/ConfigureEndpointSqlServerTransport.cs
+++ src/NServiceBus.SqlServer.AcceptanceTests/ConfigureEndpointSqlServerTransport.cs
@@ -6,7 +6,6 @@ using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.Configuration.AdvancedExtensibility;
 using NServiceBus.Transport;
-using NServiceBus.Transport.SQLServer;
 
 public class ConfigureEndpointSqlServerTransport : IConfigureEndpointTestExecution
 {
@@ -77,4 +76,106 @@ public class ConfigureEndpointSqlServerTransport : IConfigureEndpointTestExecuti
 
     string connectionString;
     QueueBindings queueBindings;
+
+    class QueueAddress
+    {
+        public QueueAddress(string table, string schemaName, string catalogName)
+        {
+            Table = table;
+            Catalog = SafeUnquote(catalogName);
+            Schema = SafeUnquote(schemaName);
+        }
+
+        public string Catalog { get; }
+        public string Table { get; }
+        public string Schema { get; }
+
+        public static QueueAddress Parse(string address)
+        {
+            var firstAtIndex = address.IndexOf("@", StringComparison.Ordinal);
+
+            if (firstAtIndex == -1)
+            {
+                return new QueueAddress(address, null, null);
+            }
+
+            var tableName = address.Substring(0, firstAtIndex);
+            address = firstAtIndex + 1 < address.Length ? address.Substring(firstAtIndex + 1) : string.Empty;
+
+            address = ExtractNextPart(address, out var schemaName);
+
+            string catalogName = null;
+
+            if (address != string.Empty)
+            {
+                ExtractNextPart(address, out catalogName);
+            }
+            return new QueueAddress(tableName, schemaName, catalogName);
+        }
+
+        public string QualifiedTableName => $"{Quote(Catalog)}.{Quote(Schema)}.{Quote(Table)}";
+
+        static string ExtractNextPart(string address, out string part)
+        {
+            var noRightBrackets = 0;
+            var index = 1;
+
+            while (true)
+            {
+                if (index >= address.Length)
+                {
+                    part = address;
+                    return string.Empty;
+                }
+
+                if (address[index] == '@' && (address[0] != '[' || noRightBrackets % 2 == 1))
+                {
+                    part = address.Substring(0, index);
+                    return index + 1 < address.Length ? address.Substring(index + 1) : string.Empty;
+                }
+
+                if (address[index] == ']')
+                {
+                    noRightBrackets++;
+                }
+
+                index++;
+            }
+        }
+
+        static string Quote(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+            return prefix + name.Replace(suffix, suffix + suffix) + suffix;
+        }
+
+        static string SafeUnquote(string name)
+        {
+            var result = Unquote(name);
+            return string.IsNullOrWhiteSpace(result)
+                ? null
+                : result;
+        }
+
+        const string prefix = "[";
+        const string suffix = "]";
+        static string Unquote(string quotedString)
+        {
+            if (quotedString == null)
+            {
+                return null;
+            }
+
+            if (!quotedString.StartsWith(prefix) || !quotedString.EndsWith(suffix))
+            {
+                return quotedString;
+            }
+
+            return quotedString
+                .Substring(prefix.Length, quotedString.Length - prefix.Length - suffix.Length).Replace(suffix + suffix, suffix);
+        }
+    }
 }
