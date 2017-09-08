e7d25c32e25940c2a82833830197b32be92ba0ab
SzymonPobiega <szymon.pobiega@gmail.com>
2017-09-08 11:17:00 +0200

    Added Directory.Build.props

--- src/NServiceBus.SqlServer/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NServiceBus.SQLServer")]
-[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus SQLServer Transport")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: AssemblyConfiguration("release")]
-[assembly: ComVisible(false)]
