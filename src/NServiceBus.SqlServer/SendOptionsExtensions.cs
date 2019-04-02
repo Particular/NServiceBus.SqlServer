54fadb504f3b0752c748010e51504cb62bf54b38
Kyle Baley <kyle@baley.org>
2019-04-02 08:17:32 -0500

    Fix obsolete SendOptions method (#484)
    * Fix obsolete SendOptions method

--- src/NServiceBus.SqlServer/SendOptionsExtensions.cs
+++ src/NServiceBus.SqlServer/SendOptionsExtensions.cs
@@ -16,9 +16,10 @@
         /// <param name="connection">Open <see cref="SqlConnection"/> instance to be used by send operations.</param>
         /// <param name="transaction"><see cref="SqlTransaction"/> instance that will be used by any operations performed by the transport.</param>
         [ObsoleteEx(
-            RemoveInVersion = "5.0",
-            TreatAsErrorFromVersion = "4.0",
-            Message = "The connection parameter is no longer required. Use the alternate overload with only the transaction parameter.")]
+            RemoveInVersion = "6.0",
+            TreatAsErrorFromVersion = "5.0",
+            ReplacementTypeOrMember = "UseCustomSqlTransaction",
+            Message = "The connection parameter is no longer required.")]
         public static void UseCustomSqlConnectionAndTransaction(this SendOptions options, SqlConnection connection, SqlTransaction transaction)
         {
             var transportTransaction = new TransportTransaction();
@@ -29,11 +30,10 @@
         }
 
         /// <summary>
-        /// Enables providing SqlConnection and SqlTransaction instances that will be used by send operations. The same connection and transaction
-        /// can be used in more than one send operation.
+        /// Enables the use of custom SqlTransaction instances for send operations. The same transaction can be used in more than one send operation.
         /// </summary>
         /// <param name="options">The <see cref="SendOptions" /> to extend.</param>
-        /// <param name="transaction">SqlTransaction instance that will be used by any operations perfromed by the transport.</param>
+        /// <param name="transaction">SqlTransaction instance that will be used by any operations performed by the transport.</param>
         public static void UseCustomSqlTransaction(this SendOptions options, SqlTransaction transaction)
         {
             var transportTransaction = new TransportTransaction();
