607b05b8c6ed308889dba57bef76ff30be61d080
Brandon Ording <bording@gmail.com>
2019-06-14 14:16:00 -0400

    Finish conversion to Particular.Packaging

--- src/NServiceBus.SqlServer/AssemblyInfo.cs
+++ src/NServiceBus.SqlServer/AssemblyInfo.cs
@@ -1,9 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NServiceBus.SQLServer")]
-[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus SQLServer Transport")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
-[assembly: AssemblyConfiguration("release")]
+
 [assembly: ComVisible(false)]
