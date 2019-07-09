3c197f9b47ce187276d560235265574553480912
WilliamBZA <william.brander@gmail.com>
2019-07-09 07:52:10 +0200

    Move timeout check later in pipeline

--- src/NServiceBus.SqlServer/DelayedDelivery/DelayedDeliveryInfrastructure.cs
+++ src/NServiceBus.SqlServer/DelayedDelivery/DelayedDeliveryInfrastructure.cs
@@ -1,11 +1,19 @@
 ﻿namespace NServiceBus.Transport.SQLServer
 {
+    using NServiceBus.Features;
+    using NServiceBus.Logging;
     using Settings;
 
     static class DelayedDeliveryInfrastructure
     {
         public static StartupCheckResult CheckForInvalidSettings(SettingsHolder settings)
         {
+            var timeoutManagerEnabled = settings.IsFeatureEnabled(typeof(TimeoutManager));
+            if (timeoutManagerEnabled)
+            {
+                Logger.Warn("Current configuration of the endpoint uses TimeoutManager feature for delayed delivery - an option which is not recommended for new deployments. SqlTransport native delayed delivery should be used instead. It can be enabled by calling `UseNativeDelayedDelivery`.");
+            }
+
             var sendOnlyEndpoint = settings.GetOrDefault<bool>("Endpoint.SendOnly");
             if (sendOnlyEndpoint)
             {
@@ -13,5 +21,7 @@
             }
             return StartupCheckResult.Success;
         }
+
+        static ILog Logger = LogManager.GetLogger("DelayedDeliveryInfrastructure");
     }
 }
