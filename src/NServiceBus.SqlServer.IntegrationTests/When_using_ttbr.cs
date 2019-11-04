67eebe5cade100cdbb54a1b1cdcacde9df1c9ed6
Mike Minutillo <mike.minutillo@particular.net>
2019-11-04 14:49:33 +0800

    Topic abstraction

--- src/NServiceBus.SqlServer.IntegrationTests/When_using_ttbr.cs
+++ src/NServiceBus.SqlServer.IntegrationTests/When_using_ttbr.cs
@@ -135,7 +135,7 @@ namespace NServiceBus.SqlServer.AcceptanceTests.TransportTransaction
 
             await PurgeOutputQueue(addressParser);
 
-            dispatcher = new MessageDispatcher(new TableBasedQueueDispatcher(sqlConnectionFactory, new TableBasedQueueOperationsReader(tableCache, null)), addressParser, new NoOpSubscriptionStore());
+            dispatcher = new MessageDispatcher(new TableBasedQueueDispatcher(sqlConnectionFactory, new TableBasedQueueOperationsReader(tableCache, null)), addressParser, new NoOpMulticastToUnicastConverter());
         }
 
         Task PurgeOutputQueue(QueueAddressTranslator addressParser)
@@ -164,21 +164,11 @@ namespace NServiceBus.SqlServer.AcceptanceTests.TransportTransaction
         const string validAddress = "TTBRTests";
 
         // TODO: Figure out if this is appropriate in this test
-        class NoOpSubscriptionStore : ISubscriptionStore
+        class NoOpMulticastToUnicastConverter : IMulticastToUnicastConverter
         {
-            public Task<List<string>> GetSubscribersForTopic(string topic)
+            public Task<List<UnicastTransportOperation>> Convert(MulticastTransportOperation transportOperation)
             {
-                return Task.FromResult(new List<string>());
-            }
-
-            public Task Subscribe(string endpointName, string endpointAddress, string topic)
-            {
-                return Task.FromResult(0);
-            }
-
-            public Task Unsubscribe(string endpointName, string topic)
-            {
-                return Task.FromResult(0);
+                return Task.FromResult(new List<UnicastTransportOperation>());
             }
         }
 
--- src/NServiceBus.SqlServer/PubSub/SubscriptionManager.cs
+++ src/NServiceBus.SqlServer/PubSub/SubscriptionManager.cs
@@ -1,44 +1,29 @@
 namespace NServiceBus.Transport.SQLServer
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using Extensibility;
 
     class SubscriptionManager : IManageSubscriptions
     {
-        public SubscriptionManager(ISubscriptionStore subscriptionStore, Func<Type, IEnumerable<Type>> typeHierarchyResolver, string endpointName, string localAddress)
+        public SubscriptionManager(ISubscriptionStore subscriptionStore, string endpointName, string localAddress)
         {
             this.subscriptionStore = subscriptionStore;
-            this.typeHierarchyResolver = typeHierarchyResolver;
             this.endpointName = endpointName;
             this.localAddress = localAddress;
         }
 
         public Task Subscribe(Type eventType, ContextBag context)
         {
-            return InvokeForEachTypeInHierarchy(eventType, topic => subscriptionStore.Subscribe(endpointName, localAddress, topic));
+            return subscriptionStore.Subscribe(endpointName, localAddress, TopicName.From(eventType));
         }
 
         public Task Unsubscribe(Type eventType, ContextBag context)
         {
-            return InvokeForEachTypeInHierarchy(eventType, topic => subscriptionStore.Unsubscribe(endpointName, topic));
-        }
-
-        Task InvokeForEachTypeInHierarchy(Type type, Func<string, Task> action)
-        {
-            var allTasks = typeHierarchyResolver(type).Select(x => action(TopicName(x))).ToArray();
-            return Task.WhenAll(allTasks);
-        }
-
-        static string TopicName(Type type)
-        {
-            return $"{type.Namespace}.{type.Name}";
+            return subscriptionStore.Unsubscribe(endpointName, TopicName.From(eventType));
         }
 
         ISubscriptionStore subscriptionStore;
-        Func<Type, IEnumerable<Type>> typeHierarchyResolver;
         string endpointName;
         string localAddress;
     }
