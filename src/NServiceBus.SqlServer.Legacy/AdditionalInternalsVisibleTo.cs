b54279d2f651d7b2ed54ec09cd035ca53303d567
danielmarbach <daniel.marbach@openplace.net>
2020-01-23 10:16:10 +0100

    Introduce legacy project

--- src/NServiceBus.SqlServer.Legacy/AdditionalInternalsVisibleTo.cs
+++ src/NServiceBus.SqlServer.Legacy/AdditionalInternalsVisibleTo.cs
@@ -1,7 +1,3 @@
-namespace NServiceBus.SqlServer.Legacy
-{
-    public class AdditionalInternalsVisibleTo
-    {
-        
-    }
-}
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NServiceBus.SqlServer.Legacy.AcceptanceTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d5a2fc697d5277272662d3154a752010b3de6d598204c10c4b09ebb28b469640efcf04978e95a15f4e0461f02316c96b349083a2e2a4f07fe7dfb713b99189b634473c73c1387149a37dbc836028bc2ca21de196bbd374f4024b920a0da86fe47bf541771352246cd8ef54d48654f39f4073aa114b70dc7d4712c3d9dd83faad")]
