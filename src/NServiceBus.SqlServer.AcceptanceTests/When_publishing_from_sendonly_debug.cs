572ce6468a49d006b2fdedf7638aad4dac16097c
lailabougria <laila.bougria@particular.net>
2020-08-26 11:05:25 +0200

    Add local copy of the test with some tweaks

---
+++ src/NServiceBus.SqlServer.AcceptanceTests/When_publishing_from_sendonly_debug.cs
@@ -0,0 +1,123 @@
+﻿namespace NServiceBus.AcceptanceTests.Routing.MessageDrivenSubscriptions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Extensibility;
+    using Features;
+    using NServiceBus;
+    using NUnit.Framework;
+    using Persistence;
+    using Unicast.Subscriptions;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    public class When_publishing_from_sendonly_debug : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_be_delivered_to_all_subscribers()
+        {
+            Requires.MessageDrivenPubSub();
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Subscriber>()
+                .WithEndpoint<SendOnlyPublisher>(b => b.When((session, c) => session.Publish(new MyEvent())))
+                .Done(c => c.SubscriberGotTheEvent)
+                .Run();
+
+            Assert.True(context.SubscriberGotTheEvent);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool SubscriberGotTheEvent { get; set; }
+        }
+
+        public class SendOnlyPublisher : EndpointConfigurationBuilder
+        {
+            public SendOnlyPublisher()
+            {
+                EndpointSetup<DefaultPublisher>(b =>
+                {
+                    b.SendOnly();
+                    b.UsePersistence(typeof(HardCodedPersistence));
+                    b.DisableFeature<AutoSubscribe>();
+                });
+            }
+        }
+
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.DisableFeature<AutoSubscribe>();
+                    c.EnableInstallers();
+                });
+            }
+
+            public class MyEventHandler : IHandleMessages<MyEvent>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MyEvent messageThatIsEnlisted, IMessageHandlerContext context)
+                {
+                    Context.SubscriberGotTheEvent = true;
+
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+
+        public class MyEvent : IEvent
+        {
+        }
+
+        public class HardCodedPersistence : PersistenceDefinition
+        {
+            internal HardCodedPersistence()
+            {
+                Supports<StorageType.Subscriptions>(s => s.EnableFeatureByDefault<HardCodedPersistenceFeature>());
+            }
+        }
+
+        public class HardCodedPersistenceFeature : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                context.Container.ConfigureComponent<HardcodedSubscriptionManager>(DependencyLifecycle.SingleInstance);
+            }
+        }
+
+        public class HardcodedSubscriptionManager : ISubscriptionStorage
+        {
+            public HardcodedSubscriptionManager()
+            {
+                addressTask = Task.FromResult(new[]
+                {
+                    new Unicast.Subscriptions.MessageDrivenSubscriptions.Subscriber("publishingFromSendonly.subscriber", null)
+                }.AsEnumerable());
+            }
+
+            public Task Subscribe(Unicast.Subscriptions.MessageDrivenSubscriptions.Subscriber subscriber, MessageType messageType, ContextBag context)
+            {
+                return Task.FromResult(0);
+            }
+
+            public Task Unsubscribe(Unicast.Subscriptions.MessageDrivenSubscriptions.Subscriber subscriber, MessageType messageType, ContextBag context)
+            {
+                return Task.FromResult(0);
+            }
+
+            public Task<IEnumerable<Unicast.Subscriptions.MessageDrivenSubscriptions.Subscriber>> GetSubscriberAddressesForMessage(IEnumerable<MessageType> messageTypes, ContextBag context)
+            {
+                return addressTask;
+            }
+
+            Task<IEnumerable<Unicast.Subscriptions.MessageDrivenSubscriptions.Subscriber>> addressTask;
+        }
+    }
+}
