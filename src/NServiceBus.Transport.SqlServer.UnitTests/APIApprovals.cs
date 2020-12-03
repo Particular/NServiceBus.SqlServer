2461d782296da9cb665dda8df900d9e01d491675
WilliamBZA <william.brander@gmail.com>
2020-12-03 08:12:46 +0200

    Exclude AssemblyMetadataAttribute

--- src/NServiceBus.Transport.SqlServer.UnitTests/APIApprovals.cs
+++ src/NServiceBus.Transport.SqlServer.UnitTests/APIApprovals.cs
@@ -9,7 +9,14 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = typeof(SqlServerTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions { ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" } });
+        var publicApi = typeof(SqlServerTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[]
+            {
+                "System.Runtime.Versioning.TargetFrameworkAttribute",
+                "System.Reflection.AssemblyMetadataAttribute"
+            }
+        });
         Approver.Verify(publicApi);
     }
 }
