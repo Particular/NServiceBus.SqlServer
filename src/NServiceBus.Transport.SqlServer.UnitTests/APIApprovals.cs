e755de285377693b5d7fddb2ada3616db6e86576
Sean Feldman <feldman.sean@gmail.com>
2020-12-14 21:12:39 -0700

    Fix API Approvals

--- src/NServiceBus.Transport.SqlServer.UnitTests/APIApprovals.cs
+++ src/NServiceBus.Transport.SqlServer.UnitTests/APIApprovals.cs
@@ -9,7 +9,7 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(SqlServerTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(SqlServerTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" });
         Approver.Verify(publicApi);
     }
 }
