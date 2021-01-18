135ddd78552d42efa4f17625f4751f8ac54cbf6c
Tomek Masternak <tomasz.masternak@particular.net>
2021-01-18 14:50:55 +0100

    tests compiling

---
+++ src/NServiceBus.Transport.SqlServer/PubSub/MessageDrivenPubSubCompatibilityModeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus.Configuration.AdvancedExtensibility;
+
+namespace NServiceBus
+{
+    /// <summary>
+    ///     Configuration extensions for Message-Driven Pub-Sub compatibility mode
+    /// </summary>
+    public static partial class MessageDrivenPubSubCompatibilityModeConfiguration
+    {
+        /// <summary>
+        ///     Enables compatibility with endpoints running on message-driven pub-sub
+        /// </summary>
+        /// <param name="routingSettings">The transport to enable pub-sub compatibility on</param>
+        public static SubscriptionMigrationModeSettings EnableMessageDrivenPubSubCompatibilityMode(
+            this RoutingSettings routingSettings)
+        {
+            var settings = routingSettings.GetSettings();
+            settings.Set("NServiceBus.Subscriptions.EnableMigrationMode", true);
+            return new SubscriptionMigrationModeSettings(settings);
+        }
+    }
+}
