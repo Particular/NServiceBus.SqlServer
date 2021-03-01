970ea4a7cd3f454f6a04494fb8b1815a8a483a85
David Boike <david.boike@gmail.com>
2021-03-01 13:25:47 -0600

    Temporary version of transport test to show it passes until next alpha

--- src/NServiceBus.SqlServer.TransportTests/TempTransportTest.cs
+++ src/NServiceBus.SqlServer.TransportTests/TempTransportTest.cs
@@ -69,3 +69,46 @@
         }
     }
 }
+
+namespace NServiceBus.TransportTests
+{
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_transaction_level_TransactionScope_DEBUG : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_have_active_transaction(TransportTransactionMode transactionMode)
+        {
+            Transaction currentTransaction = null;
+            Transaction contextTransaction = null;
+
+            var completed = new TaskCompletionSource<bool>();
+            OnTestTimeout(() => completed.SetCanceled());
+
+            await StartPump(
+                (context, _) =>
+                {
+                    currentTransaction = Transaction.Current;
+                    contextTransaction = context.TransportTransaction.Get<Transaction>();
+                    return Task.CompletedTask;
+                },
+                (_, __) => Task.FromResult(ErrorHandleResult.Handled),
+                (_, __) =>
+                {
+                    completed.SetResult(true);
+                    return Task.CompletedTask;
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName);
+
+            _ = await completed.Task;
+
+            Assert.NotNull(currentTransaction);
+            Assert.AreSame(currentTransaction, contextTransaction);
+        }
+    }
+}
