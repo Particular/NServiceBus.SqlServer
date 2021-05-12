1fa8aeec393a9dc86f1182292bcbe4d23b8a9f17
Adam Ralph <adam@adamralph.com>
2021-05-12 10:46:25 +0200

    wip

--- src/NServiceBus.Transport.SqlServer/Foo.cs
+++ src/NServiceBus.Transport.SqlServer/Foo.cs
@@ -1,21 +1,45 @@
-﻿namespace NServiceBus.Transport.SqlServer
+﻿#pragma warning disable MA0048 // File name must match type name
+namespace NServiceBus.Transport.SqlServer
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading;
+    using System.Threading.Tasks;
 
     class Foo
     {
+        public Foo()
+        { }
+
+        public Foo(CancellationToken cancellationToken) { }
+
         protected void Bar<T>() { Bar(CancellationToken.None); }
         protected void Bar(CancellationToken cancellationToken = default) { _ = GetType(); }
     }
 
     class Test : Foo
     {
-        public void Something(CancellationToken cancellationToken = default)
+        public async Task Something(CancellationToken cancellationToken)
         {
-            Bar(cancellationToken);
+
+            _ = new Foo();
+            Bar();
 
             Extensions.Baz(123);
+
+            await foreach (var item in GenerateSequenceAsync().ConfigureAwait(false))
+            {
+                Console.WriteLine(item);
+            }
+
+            Func<CancellationToken, bool> foo = token =>
+                {
+                    Bar();
+                    return true;
+                }
         }
+
+        IAsyncEnumerable<object> GenerateSequenceAsync() => throw new NotSupportedException();
     }
 
     static class Extensions
