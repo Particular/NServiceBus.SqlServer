7cae3de2905da806e97d6c6f240d962431978b68
David Boike <david.boike@gmail.com>
2021-06-23 12:36:46 -0500

    Update test for NServiceBus 8

--- src/NServiceBus.Transport.SqlServer.AcceptanceTests/When_deferring_multiple_messages.cs
+++ src/NServiceBus.Transport.SqlServer.AcceptanceTests/When_deferring_multiple_messages.cs
@@ -4,7 +4,6 @@
     using System.Threading.Tasks;
     using AcceptanceTesting;
     using EndpointTemplates;
-    using Features;
     using NUnit.Framework;
 
     public class When_deferring_multiple_messages : NServiceBusAcceptanceTest
@@ -49,7 +48,7 @@
         {
             public Endpoint()
             {
-                EndpointSetup<DefaultServer>(config => config.EnableFeature<TimeoutManager>());
+                EndpointSetup<DefaultServer>();
             }
 
             public class MyMessageHandler : IHandleMessages<MyMessage>
