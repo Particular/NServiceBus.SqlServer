4af4a7342211e78590d117d07a41bebaf572e81c
Ramon Smits <ramon.smits@gmail.com>
2023-05-17 12:07:11 +0200

    Extended native pub sub scneario to include v5

---
+++ src/WireCompatibilityTests.TestBehaviors.V5/PubSubNativeSimple/Publisher.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using TestLogicApi;
+
+class Publisher : Base, ITestBehavior, IPublisher
+{
+    public Publisher() : base("Publisher")
+    {
+    }
+
+    public override async Task Execute(IEndpointInstance endpointInstance, CancellationToken cancellationToken = default)
+    {
+        await endpointInstance.Publish(new MyEvent()).ConfigureAwait(false);
+    }
+
+    public class MyEventHandler : IHandleMessages<MyEvent>
+    {
+        public Task Handle(MyEvent message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/WireCompatibilityTests.TestBehaviors.V5/PubSubNativeSimple/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using TestLogicApi;
+
+class Subscriber : Base, ITestBehavior, ISubscriber
+{
+    public Subscriber() : base("Subscriber")
+    {
+    }
+
+    public override Task Execute(IEndpointInstance endpointInstance, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public class MyEventHandler : IHandleMessages<MyEvent>
+    {
+        public Task Handle(MyEvent message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
