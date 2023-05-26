a73a70301d1f49c293fb34d5a43e17d070e91318
Ramon Smits <ramon.smits@gmail.com>
2023-05-26 09:51:16 +0200

    Dropping all tables with test run specific prefix during run initialization

--- src/WireCompatibilityTests/SqlHelper.cs
+++ src/WireCompatibilityTests/SqlHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
+using TestSuite;
 
 public static class SqlHelper
 {
@@ -42,5 +43,31 @@ if(db_id('{database}') is null)
     create database [{database}]
 ";
         await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+    }
+    public static async Task DropTableIfExists(string connectionString, string tableName, string schema = "dbo", CancellationToken cancellationToken = default)
+    {
+        await ExecuteSql(Global.ConnectionString, $"DROP TABLE IF EXISTS [{schema}].[{tableName}]", cancellationToken).ConfigureAwait(false);
+    }
+
+    public static async Task DropTablesWithPrefix(string connectionString, string prefix, CancellationToken cancellationToken = default)
+    {
+        var sql = @$"
+DECLARE @cmd varchar(4000)
+DECLARE cmds CURSOR FOR
+SELECT 'DROP TABLE [' + TABLE_SCHEMA +'].[' + TABLE_NAME + ']' 
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_NAME LIKE '{prefix}%'
+
+OPEN cmds
+WHILE 1 = 1
+BEGIN
+    FETCH cmds INTO @cmd
+    IF @@fetch_status != 0 BREAK
+    EXEC(@cmd)
+END
+CLOSE cmds;
+DEALLOCATE cmds";
+
+        await ExecuteSql(connectionString, sql, cancellationToken).ConfigureAwait(false);
     }
 }
