4b2c712b185e3b95ae69c34ebdfea821a2e8fd77
Andreas Öhlund <andreas.ohlund@particular.net>
2023-09-18 12:46:54 +0200

    Set proper default

--- src/NServiceBus.Transport.SqlServer/SqlServerTransport.cs
+++ src/NServiceBus.Transport.SqlServer/SqlServerTransport.cs
@@ -21,7 +21,7 @@ namespace NServiceBus
         /// Creates and instance of <see cref="SqlServerTransport"/>
         /// </summary>
         public SqlServerTransport(string connectionString)
-            : base(TransportTransactionMode.TransactionScope, true, true, true)
+            : base(TransportTransactionMode.SendsAtomicWithReceive, true, true, true)
         {
             Guard.AgainstNullAndEmpty(nameof(connectionString), connectionString);
 
@@ -33,7 +33,7 @@ namespace NServiceBus
         /// </summary>
         /// <param name="connectionFactory">Connection factory that returns an instance of <see cref="SqlConnection"/> in an Opened state.</param>
         public SqlServerTransport(Func<CancellationToken, Task<SqlConnection>> connectionFactory)
-            : base(TransportTransactionMode.TransactionScope, true, true, true)
+            : base(TransportTransactionMode.SendsAtomicWithReceive, true, true, true)
         {
             Guard.AgainstNull(nameof(connectionFactory), connectionFactory);
 
@@ -44,7 +44,7 @@ namespace NServiceBus
         /// Used for backwards compatibility with the legacy transport api.
         /// </summary>
         internal SqlServerTransport()
-            : base(TransportTransactionMode.TransactionScope, true, true, true)
+            : base(TransportTransactionMode.SendsAtomicWithReceive, true, true, true)
         {
         }
 
@@ -52,7 +52,7 @@ namespace NServiceBus
         /// For the pub-sub migration tests only
         /// </summary>
         internal SqlServerTransport(string connectionString, bool supportsDelayedDelivery = true, bool supportsPublishSubscribe = true)
-            : base(TransportTransactionMode.TransactionScope, supportsDelayedDelivery, supportsPublishSubscribe, true)
+            : base(TransportTransactionMode.SendsAtomicWithReceive, supportsDelayedDelivery, supportsPublishSubscribe, true)
         {
             ConnectionString = connectionString;
         }
