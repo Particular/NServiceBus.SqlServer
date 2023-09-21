3d11f63cecd77f4d4197884d9da1c040849bc636
Brandon Ording <bording@gmail.com>
2023-09-21 15:13:24 -0400

    Remove pragmas and use new language features in SubscriptionManager

--- src/NServiceBus.Transport.SqlServer/PubSub/SubscriptionManager.cs
+++ src/NServiceBus.Transport.SqlServer/PubSub/SubscriptionManager.cs
@@ -6,34 +6,12 @@ namespace NServiceBus.Transport.SqlServer
     using Extensibility;
     using NServiceBus.Unicast.Messages;
 
-    class SubscriptionManager : ISubscriptionManager
+    class SubscriptionManager(ISubscriptionStore subscriptionStore, string endpointName, string localAddress) : ISubscriptionManager
     {
-        public SubscriptionManager(ISubscriptionStore subscriptionStore, string endpointName, string localAddress)
-        {
-            this.subscriptionStore = subscriptionStore;
-            this.endpointName = endpointName;
-            this.localAddress = localAddress;
-        }
+        public Task Subscribe(MessageMetadata eventType, CancellationToken cancellationToken = default) => subscriptionStore.Subscribe(endpointName, localAddress, eventType.MessageType, cancellationToken);
 
-#pragma warning disable IDE0060 // Remove unused parameter
-        public Task Subscribe(MessageMetadata eventType, ContextBag context, CancellationToken cancellationToken = default)
-#pragma warning restore IDE0060 // Remove unused parameter
-        {
-            return subscriptionStore.Subscribe(endpointName, localAddress, eventType.MessageType, cancellationToken);
-        }
+        public Task SubscribeAll(MessageMetadata[] eventTypes, ContextBag context, CancellationToken cancellationToken = default) => Task.WhenAll(eventTypes.Select(et => Subscribe(et, cancellationToken)));
 
-        public Task SubscribeAll(MessageMetadata[] eventTypes, ContextBag context, CancellationToken cancellationToken = default)
-        {
-            return Task.WhenAll(eventTypes.Select(et => Subscribe(et, context, cancellationToken)));
-        }
-
-        public Task Unsubscribe(MessageMetadata eventType, ContextBag context, CancellationToken cancellationToken = default)
-        {
-            return subscriptionStore.Unsubscribe(endpointName, eventType.MessageType, cancellationToken);
-        }
-
-        ISubscriptionStore subscriptionStore;
-        string endpointName;
-        string localAddress;
+        public Task Unsubscribe(MessageMetadata eventType, ContextBag context, CancellationToken cancellationToken = default) => subscriptionStore.Unsubscribe(endpointName, eventType.MessageType, cancellationToken);
     }
 }
