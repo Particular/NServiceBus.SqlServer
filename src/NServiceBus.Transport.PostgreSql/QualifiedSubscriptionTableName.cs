934e2ab1c00d3b512a0e2c38f2389199dacd898f
Tomek Masternak <tomasz.masternak@particular.net>
2024-01-23 13:59:06 +0100

    subscribtion config

---
+++ src/NServiceBus.Transport.PostgreSql/QualifiedSubscriptionTableName.cs
@@ -0,0 +1,32 @@
+﻿namespace NServiceBus.Transport.PostgreSql
+{
+    using System;
+    using static SqlServer.NameHelper;
+
+    class QualifiedSubscriptionTableName
+    {
+        public string QuotedCatalog;
+        public string QuotedQualifiedName;
+
+        public QualifiedSubscriptionTableName(string table, string schema, string catalog)
+        {
+            if (table == null)
+            {
+                throw new ArgumentNullException(nameof(table));
+            }
+
+            if (schema == null)
+            {
+                throw new ArgumentNullException(nameof(schema));
+            }
+
+            if (catalog == null)
+            {
+                throw new ArgumentNullException(nameof(catalog));
+            }
+
+            QuotedCatalog = Quote(catalog);
+            QuotedQualifiedName = $"{Quote(catalog)}.{Quote(schema)}.{Quote(table)}";
+        }
+    }
+}
