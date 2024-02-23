64ef4fbb6307bd284ed276b179bc7ea626f28fe2
Dennis <dvdstelt@gmail.com>
2024-02-23 09:54:50 +0100

    Extracted backoffstrategy

---
+++ src/NServiceBus.Transport.Sql.Shared/DelayedDelivery/BackOffStrategy.cs
@@ -0,0 +1,92 @@
+﻿namespace NServiceBus.Transport.Sql.Shared.DelayedDelivery;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Logging;
+
+public class BackOffStrategy
+{
+    public void RegisterNewDueTime(DateTime dueTime)
+    {
+        NextDelayedMessage = dueTime;
+        if (dueTime == DateTime.MinValue)
+        {
+            Logger.Debug("No delayed messages available...");
+            DelayedMessageAvailable = false;
+            return;
+        }
+
+        if (dueTime < NextExecutionTime)
+        {
+            Logger.Debug("New delayed message registered with dueTime earlier than previously known next dueTime");
+            DelayedMessageAvailable = true;
+            NextExecutionTime = dueTime;
+            return;
+        }
+
+        Logger.Debug("Delayed messages found but dueTime is later than time to peek for new delayed messages.");
+        DelayedMessageAvailable = true;
+    }
+
+
+    public async Task WaitForNextExecution(CancellationToken cancellationToken = new())
+    {
+        // Whoops, we should've already moved the delayed message! Quickly, move on! :-)
+        if (AreDelayedMessagesMatured)
+        {
+            Logger.Debug(
+                "Scheduling next attempt to move matured delayed messages immediately because a full batch was detected.");
+            return;
+        }
+
+        CalculateNextExecutionTime();
+
+        // While running this loop, a new delayed message can be stored
+        // and NextExecutionTime could be set to a new (sooner) time.
+        while (DateTime.UtcNow < NextExecutionTime)
+        {
+            await Task.Delay(1000, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    public void CalculateNextExecutionTime()
+    {
+        IncreaseExponentialBackOff();
+
+        var newExecutionTime = DateTime.UtcNow.AddMilliseconds(milliseconds);
+
+        // If the next delayed message is coming up before the back-off time, use that.
+        if (DelayedMessageAvailable && newExecutionTime > NextDelayedMessage)
+        {
+            Logger.Debug(
+                $"Scheduling next attempt to move matured delayed messages for time of next message due at {NextDelayedMessage}.");
+            NextExecutionTime = NextDelayedMessage;
+            // We find a better time to execute, so we can reset the exponential back off
+            milliseconds = 500;
+            return;
+        }
+
+        Logger.Debug(
+            $"Exponentially backing off for {milliseconds / 1000} seconds until {newExecutionTime}.");
+        NextExecutionTime = newExecutionTime;
+    }
+
+    void IncreaseExponentialBackOff()
+    {
+        milliseconds *= 2;
+        if (milliseconds > 60000)
+        {
+            milliseconds = 60000;
+        }
+    }
+
+    bool AreDelayedMessagesMatured => DelayedMessageAvailable && NextExecutionTime < DateTime.UtcNow;
+
+    DateTime NextDelayedMessage { get; set; } = DateTime.UtcNow;
+    public DateTime NextExecutionTime { get; private set; } = DateTime.UtcNow.AddSeconds(2);
+    bool DelayedMessageAvailable { get; set; }
+
+    int milliseconds = 500; // First time multiplied will be 1 second.
+    static readonly ILog Logger = LogManager.GetLogger<BackOffStrategy>();
+}
--- src/NServiceBus.Transport.SqlServer/DelayedDelivery/DueDelayedMessageProcessor.cs
+++ src/NServiceBus.Transport.SqlServer/DelayedDelivery/DueDelayedMessageProcessor.cs
@@ -4,6 +4,7 @@ namespace NServiceBus.Transport.SqlServer
     using System.Threading;
     using System.Threading.Tasks;
     using Logging;
+    using Sql.Shared.DelayedDelivery;
 
     class DueDelayedMessageProcessor
     {
@@ -15,7 +16,6 @@ namespace NServiceBus.Transport.SqlServer
             this.table = table;
             this.connectionFactory = connectionFactory;
             this.batchSize = batchSize;
-            nextExecution = DateTime.MinValue;
 
             table.OnStoreDelayedMessage += OnDelayedMessageStored;
         }
@@ -58,11 +58,11 @@ namespace NServiceBus.Transport.SqlServer
                     try
                     {
                         var nextDueTime = await ExecuteOnce(moveDelayedMessagesCancellationToken).ConfigureAwait(false);
-                        whenToRunNext.SetNextDelayedMessage(nextDueTime);
+                        backOffStrategy.RegisterNewDueTime(nextDueTime);
 
                         dueDelayedMessageProcessorCircuitBreaker.Success();
 
-                        await WaitForNextExecution(moveDelayedMessagesCancellationToken)
+                        await backOffStrategy.WaitForNextExecution(moveDelayedMessagesCancellationToken)
                             .ConfigureAwait(false);
                     }
                     catch (Exception ex) when (!ex.IsCausedBy(moveDelayedMessagesCancellationToken))
@@ -100,36 +100,13 @@ namespace NServiceBus.Transport.SqlServer
             }
         }
 
-        async Task WaitForNextExecution(CancellationToken moveDelayedMessagesCancellationToken)
-        {
-            // Whoops, we should've already moved the delayed message! Quickly, move on! :-)
-            if (whenToRunNext.AreDelayedMessagesMatured)
-            {
-                Logger.Debug(
-                    "Scheduling next attempt to move matured delayed messages immediately because a full batch was detected.");
-                return;
-            }
-
-            whenToRunNext.CalculateNextExecutionTime();
-            // While running this loop, a new delayed message can be stored
-            // and NextExecutionTime could be set to a new (sooner) time.
-            while (DateTime.UtcNow < whenToRunNext.NextExecutionTime)
-            {
-                await Task.Delay(1000, moveDelayedMessagesCancellationToken).ConfigureAwait(false);
-            }
-        }
-
         /// <summary>
         /// If a delayed message is stored with a dueTime lower than nextExecution time, then reset nextExecutionTime
         /// </summary>
         void OnDelayedMessageStored(object sender, DateTime dueTime)
         {
             Logger.Debug($"Delayed messages stored which is due at {dueTime}");
-
-            if (dueTime < nextExecution)
-            {
-                nextExecution = dueTime;
-            }
+            backOffStrategy.RegisterNewDueTime(dueTime);
         }
 
         readonly TimeSpan waitTimeCircuitBreaker;
@@ -142,67 +119,8 @@ namespace NServiceBus.Transport.SqlServer
         CancellationTokenSource moveDelayedMessagesCancellationTokenSource;
         Task moveDelayedMessagesTask;
         RepeatedFailuresOverTimeCircuitBreaker dueDelayedMessageProcessorCircuitBreaker;
-        DateTime nextExecution;
 
         static readonly ILog Logger = LogManager.GetLogger<DueDelayedMessageProcessor>();
-        WhenToRunNext whenToRunNext = new();
-
-        class WhenToRunNext
-        {
-            public void SetNextDelayedMessage(DateTime dueDate)
-            {
-                NextDelayedMessage = dueDate;
-                if (dueDate == DateTime.MinValue)
-                {
-                    Logger.Debug("No delayed messages available...");
-                    DelayedMessageAvailable = false;
-                    return;
-                }
-
-                Logger.Debug($"Delayed message found with due date of {dueDate}");
-                DelayedMessageAvailable = true;
-                NextExecutionTime = dueDate;
-            }
-
-            public void CalculateNextExecutionTime()
-            {
-                IncreaseExponentialBackOff();
-
-                var newExecutionTime = DateTime.UtcNow.AddMilliseconds(milliseconds);
-
-                // If the next delayed message is coming up before the back-off time, use that.
-                if (DelayedMessageAvailable && newExecutionTime > NextDelayedMessage)
-                {
-                    Logger.Debug(
-                        $"Scheduling next attempt to move matured delayed messages for time of next message due at {NextDelayedMessage}.");
-                    NextExecutionTime = NextDelayedMessage;
-                    // We find a better time to execute, so we can reset the exponential back off
-                    milliseconds = 500;
-                    return;
-                }
-
-                Logger.Debug(
-                    $"Exponentially backing off for {milliseconds / 1000} seconds until {newExecutionTime}.");
-                NextExecutionTime = newExecutionTime;
-            }
-
-            void IncreaseExponentialBackOff()
-            {
-                milliseconds *= 2;
-                if (milliseconds > 60000)
-                {
-                    milliseconds = 60000;
-                }
-            }
-
-            public bool AreDelayedMessagesMatured =>
-                DelayedMessageAvailable && NextExecutionTime < DateTime.UtcNow;
-
-            DateTime NextDelayedMessage { get; set; } = DateTime.UtcNow;
-            public DateTime NextExecutionTime { get; private set; } = DateTime.UtcNow.AddSeconds(2);
-            bool DelayedMessageAvailable { get; set; }
-
-            int milliseconds = 500; // First time multiplied will be 1 second.
-        }
+        BackOffStrategy backOffStrategy = new();
     }
 }
