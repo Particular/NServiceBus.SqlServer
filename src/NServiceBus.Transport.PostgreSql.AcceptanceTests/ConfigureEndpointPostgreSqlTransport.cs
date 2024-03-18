44a029f9b379d26efd3d30e0df4977d8bdce937f
Szymon Pobiega <szymon.pobiega@gmail.com>
2024-03-18 12:53:01 +0100

    Add acceptance tests

---
+++ src/NServiceBus.Transport.PostgreSql.AcceptanceTests/ConfigureEndpointPostgreSqlTransport.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Transport.PostgreSql;
+
+public class ConfigureEndpointPostgreSqlTransport : IConfigureEndpointTestExecution
+{
+    public ConfigureEndpointPostgreSqlTransport(PostgreSqlTransport transport)
+    {
+        this.transport = transport;
+    }
+
+    public ConfigureEndpointPostgreSqlTransport()
+    {
+        connectionString = Environment.GetEnvironmentVariable("PostgreSqlTransportConnectionString") ?? @"User ID=user;Password=admin;Host=localhost;Port=54320;Database=nservicebus;Pooling=true;Connection Lifetime=0;";
+
+        transport = new PostgreSqlTransport(connectionString);
+        transport.Subscriptions.DisableCaching = true;
+
+        //On non windows operating systems we need to explicitly set the transaction mode to SendsAtomicWithReceive since distributed transactions is not available there
+        if (!OperatingSystem.IsWindows() && transport.TransportTransactionMode == TransportTransactionMode.TransactionScope)
+        {
+            transport.TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive;
+        }
+    }
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings runSettings, PublisherMetadata publisherMetadata)
+    {
+        doNotCleanNativeSubscriptions = runSettings.TryGet<bool>("DoNotCleanNativeSubscriptions", out _);
+
+        configuration.UseTransport(transport);
+        this.endpointName = endpointName;
+
+        return Task.FromResult(0);
+    }
+
+    public async Task Cleanup()
+    {
+        using (var conn = new NpgsqlConnection(connectionString))
+        {
+            await conn.OpenAsync().ConfigureAwait(false);
+
+            var queueAddresses = transport.Testing.ReceiveAddresses;
+            var delayedQueueAddress = transport.Testing.DelayedDeliveryQueue;
+
+            var commandTextBuilder = new StringBuilder();
+
+            //No clean-up for send-only endpoints
+            if (queueAddresses != null)
+            {
+                foreach (var address in queueAddresses)
+                {
+                    commandTextBuilder.AppendLine($"DROP TABLE IF EXISTS {address};");
+                }
+            }
+
+            commandTextBuilder.AppendLine($"DROP TABLE IF EXISTS {delayedQueueAddress};");
+
+            var subscriptionTableName = transport.Testing.SubscriptionTable;
+
+            if (!doNotCleanNativeSubscriptions && !string.IsNullOrEmpty(subscriptionTableName))
+            {
+                commandTextBuilder.AppendLine($"DROP TABLE IF EXISTS {subscriptionTableName};");
+            }
+
+            var commandText = commandTextBuilder.ToString();
+            if (!string.IsNullOrEmpty(commandText))
+            {
+                await TryDeleteTables(conn, commandText);
+            }
+        }
+    }
+
+    async Task TryDeleteTables(NpgsqlConnection conn, string commandText)
+    {
+        try
+        {
+            using (var comm = conn.CreateCommand())
+            {
+                comm.CommandText = commandText;
+                await comm.ExecuteNonQueryAsync().ConfigureAwait(false);
+            }
+        }
+        catch (Exception e)
+        {
+            if (!e.Message.Contains("it does not exist or you do not have permission"))
+            {
+                throw new Exception($"Failed to execute query in {endpointName}: {commandText}", e);
+            }
+        }
+    }
+
+    bool doNotCleanNativeSubscriptions;
+    PostgreSqlTransport transport;
+    string connectionString;
+    string endpointName;
+}
