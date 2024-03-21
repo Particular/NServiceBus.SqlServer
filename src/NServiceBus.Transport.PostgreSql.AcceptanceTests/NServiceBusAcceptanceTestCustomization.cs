b0b639eecb7132f31aa433d5dfcf8c58c9d70ba3
Poornima Nayar <poornimakrishnav@gmail.com>
2024-03-21 12:07:57 +0000

    Working on PostgreSQL identifier limits

---
+++ src/NServiceBus.Transport.PostgreSql.AcceptanceTests/NServiceBusAcceptanceTestCustomization.cs
@@ -0,0 +1,75 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+    using NUnit.Framework.Internal;
+
+    /// <summary>
+    /// Base class for all the NSB test that sets up our conventions
+    /// </summary>
+    [TestFixture]
+    public abstract partial class NServiceBusAcceptanceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Conventions.EndpointNamingConvention = t =>
+            {
+                var classAndEndpoint = t.FullName.Split('.').Last();
+
+                var testName = classAndEndpoint.Split('+').First();
+
+                testName = testName.Replace("When_", "");
+
+                var endpointBuilder = classAndEndpoint.Split('+').Last();
+
+                testName = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(testName);
+
+                testName = testName.Replace("_", "");
+
+                var fullTestName = testName + "." + endpointBuilder;
+
+                var charactersToConsider = fullTestName.Length > 63 ? 55 : fullTestName.Length;
+
+                return fullTestName.Substring(0, charactersToConsider) + fullTestName.GetHashCode().ToString("X8");
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (!TestExecutionContext.CurrentContext.TryGetRunDescriptor(out var runDescriptor))
+            {
+                return;
+            }
+
+            var scenarioContext = runDescriptor.ScenarioContext;
+
+            if (Environment.GetEnvironmentVariable("CI") != "true" || Environment.GetEnvironmentVariable("VERBOSE_TEST_LOGGING")?.ToLower() == "true")
+            {
+                TestContext.WriteLine($@"Test settings:
+{string.Join(Environment.NewLine, runDescriptor.Settings.Select(setting => $"   {setting.Key}: {setting.Value}"))}");
+
+                TestContext.WriteLine($@"Context:
+{string.Join(Environment.NewLine, scenarioContext.GetType().GetProperties().Select(p => $"{p.Name} = {p.GetValue(scenarioContext, null)}"))}");
+            }
+
+            if (TestExecutionContext.CurrentContext.CurrentResult.ResultState == ResultState.Failure || TestExecutionContext.CurrentContext.CurrentResult.ResultState == ResultState.Error)
+            {
+                TestContext.WriteLine(string.Empty);
+                TestContext.WriteLine($"Log entries (log level: {scenarioContext.LogLevel}):");
+                TestContext.WriteLine("--- Start log entries ---------------------------------------------------");
+                foreach (var logEntry in scenarioContext.Logs)
+                {
+                    TestContext.WriteLine($"{logEntry.Timestamp:T} {logEntry.Level} {logEntry.Endpoint ?? TestContext.CurrentContext.Test.Name}: {logEntry.Message}");
+                }
+                TestContext.WriteLine("--- End log entries ---------------------------------------------------");
+            }
+        }
+    }
+}
