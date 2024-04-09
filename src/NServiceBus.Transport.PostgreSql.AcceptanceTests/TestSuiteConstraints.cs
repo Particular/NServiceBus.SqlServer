0f6963d65f65e0c9fcc0c05c50205029e1f2717f
Tomek Masternak <tomasz.masternak@particular.net>
2024-04-09 13:10:41 +0200

    more tests

--- src/NServiceBus.Transport.PostgreSql.AcceptanceTests/TestSuiteConstraints.cs
+++ src/NServiceBus.Transport.PostgreSql.AcceptanceTests/TestSuiteConstraints.cs
@@ -5,7 +5,7 @@ namespace NServiceBus.AcceptanceTests
 
     public partial class TestSuiteConstraints
     {
-        public bool SupportsDtc => false; //OperatingSystem.IsWindows();
+        public bool SupportsDtc => false;
         public bool SupportsCrossQueueTransactions => true;
         public bool SupportsNativePubSub => true;
         public bool SupportsDelayedDelivery => true;
