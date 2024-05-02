c6cdf91bc4e1bafa7648fc19db87567e21331b7a
Tomek Masternak <tomasz.masternak@particular.net>
2024-05-02 13:32:48 +0200

    remove unused test-related extension point

---
+++ src/NServiceBus.Transport.PostgreSql.UnitTests/APIApprovals.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    public void Approve()
+    {
+        var publicApi = typeof(SqlServerTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[]
+            {
+                "System.Runtime.Versioning.TargetFrameworkAttribute",
+                "System.Reflection.AssemblyMetadataAttribute"
+            }
+        });
+        Approver.Verify(publicApi);
+    }
+}
