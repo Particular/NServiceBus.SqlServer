8b508ede66ddd5747be412f33d2c7392763b1ec1
williambza <william.brander@gmail.com>
2024-11-04 15:44:50 +0200

    Don't use c#7.3 features

--- src/NServiceBus.Transport.SqlServer.AcceptanceTests/MultiCatalog/When_catalog_with_special_characters_configured_for_endpoint.cs
+++ src/NServiceBus.Transport.SqlServer.AcceptanceTests/MultiCatalog/When_catalog_with_special_characters_configured_for_endpoint.cs
@@ -1,46 +1,54 @@
-﻿namespace NServiceBus.Transport.SqlServer.AcceptanceTests.MultiCatalog;
-
-using System.Threading.Tasks;
-using AcceptanceTesting;
-using NUnit.Framework;
-
-public class When_catalog_with_special_characters_configured_for_endpoint : MultiCatalogAcceptanceTest
+﻿namespace NServiceBus.Transport.SqlServer.AcceptanceTests.MultiCatalog
 {
-    static string EndpointConnectionString => WithCustomCatalog(GetDefaultConnectionString(), "n service.bus&#");
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NUnit.Framework;
 
-    [Test]
-    public async Task Should_be_able_to_send_messages_to_the_endpoint()
+    public class When_catalog_with_special_characters_configured_for_endpoint : MultiCatalogAcceptanceTest
     {
-        await Scenario.Define<Context>()
-            .WithEndpoint<AnEndpoint>(c => c.When(s => s.SendLocal(new Message())))
-            .Done(c => c.MessageReceived)
-            .Run();
+        static string EndpointConnectionString => WithCustomCatalog(GetDefaultConnectionString(), "n service.bus&#");
 
-        Assert.Pass();
-    }
-
-    public class AnEndpoint : EndpointConfigurationBuilder
-    {
-        public AnEndpoint() =>
-            EndpointSetup(new CustomizedServer(EndpointConnectionString), (_, _) => { });
-
-        class Handler(Context scenarioContext) : IHandleMessages<Message>
+        [Test]
+        public async Task Should_be_able_to_send_messages_to_the_endpoint()
         {
-            public Task Handle(Message message, IMessageHandlerContext context)
-            {
-                scenarioContext.MessageReceived = true;
+            await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(c => c.When(s => s.SendLocal(new Message())))
+                .Done(c => c.MessageReceived)
+                .Run();
 
-                return Task.FromResult(0);
+            Assert.Pass();
+        }
+
+        public class AnEndpoint : EndpointConfigurationBuilder
+        {
+            public AnEndpoint() =>
+                EndpointSetup(new CustomizedServer(EndpointConnectionString), (_, __) => { });
+
+            class Handler : IHandleMessages<Message>
+            {
+                public Handler(Context scenarioContext)
+                {
+                    ScenarioContext = scenarioContext;
+                }
+
+                public Task Handle(Message message, IMessageHandlerContext context)
+                {
+                    ScenarioContext.MessageReceived = true;
+
+                    return Task.FromResult(0);
+                }
+
+                Context ScenarioContext;
             }
         }
-    }
 
-    public class Message : ICommand
-    {
-    }
+        public class Message : ICommand
+        {
+        }
 
-    class Context : ScenarioContext
-    {
-        public bool MessageReceived { get; set; }
+        class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+        }
     }
 }
